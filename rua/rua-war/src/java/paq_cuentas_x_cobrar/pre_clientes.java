f1d761a015c90e7801b646f15055fb00579bbaec
Diego Jácome <diego10j.89@gmail.com>
2016-01-12 17:28:14 -0500

    Clientes CXC Contabilidad

--- rua/rua-war/src/java/paq_cuentas_x_cobrar/pre_clientes.java
+++ rua/rua-war/src/java/paq_cuentas_x_cobrar/pre_clientes.java
@@ -5,17 +5,22 @@
  */
 package paq_cuentas_x_cobrar;
 
+import framework.aplicacion.TablaGenerica;
 import framework.componentes.AutoCompletar;
 import framework.componentes.Boton;
+import framework.componentes.Calendario;
 import framework.componentes.Etiqueta;
 import framework.componentes.Grid;
 import framework.componentes.Grupo;
 import framework.componentes.MenuPanel;
 import framework.componentes.PanelTabla;
 import framework.componentes.Tabla;
+import framework.componentes.Texto;
 import javax.ejb.EJB;
+import org.primefaces.component.fieldset.Fieldset;
 import org.primefaces.event.SelectEvent;
-import servicios.cuentas_x_pagar.ServicioCliente;
+import servicios.contabilidad.ServicioContabilidad;
+import servicios.cuentas_x_cobrar.ServicioCliente;
 import sistema.aplicacion.Pantalla;
 
 /**
@@ -30,9 +35,18 @@ public class pre_clientes extends Pantalla {
     private final MenuPanel mep_menu = new MenuPanel();
     private AutoCompletar aut_clientes = new AutoCompletar();
 
+    //Consultas
+    private Calendario cal_fecha_inicio;
+    private Calendario cal_fecha_fin;
+
     private Tabla tab_cliente; //formulario cliente    
     private Tabla tab_transacciones_cxc; //transacciones cliente
     private Tabla tab_productos; //transacciones cliente
+
+    /*CONTABILIDAD*/
+    @EJB
+    private final ServicioContabilidad ser_contabilidad = (ServicioContabilidad) utilitario.instanciarEJB(ServicioContabilidad.class);
+    private AutoCompletar aut_cuentas;
 
     public pre_clientes() {
         aut_clientes.setId("aut_clientes");
@@ -48,9 +62,13 @@ public class pre_clientes extends Pantalla {
         bar_botones.agregarBoton(bot_clean);
 
         mep_menu.setMenuPanel("OPCIONES CLIENTE", "20%");
-        mep_menu.agregarItem("Datos Cliente", "dibujarCliente", "ui-icon-person");
+        mep_menu.agregarItem("Información Cliente", "dibujarCliente", "ui-icon-person");
         mep_menu.agregarItem("Transacciones Cliente", "dibujarTransacciones", "ui-icon-contact");
         mep_menu.agregarItem("Productos Cliente", "dibujarProductos", "ui-icon-cart");
+        mep_menu.agregarSubMenu("CONTABILIDAD");
+        mep_menu.agregarItem("Configura Cuenta Contable", "dibujarConfiguraCuenta", "ui-icon-wrench");
+        mep_menu.agregarItem("Movimientos Contables", "dibujarMovimientos", "ui-icon-note");
+
         agregarComponente(mep_menu);
 
     }
@@ -63,7 +81,25 @@ public class pre_clientes extends Pantalla {
     public void seleccionarCliente(SelectEvent evt) {
         aut_clientes.onSelect(evt);
         if (aut_clientes != null) {
-            dibujarCliente();
+            switch (mep_menu.getOpcion()) {
+                case 1:
+                    dibujarCliente();
+                    break;
+                case 2:
+                    dibujarTransacciones();
+                    break;
+                case 3:
+                    dibujarProductos();
+                    break;
+                case 4:
+                    dibujarConfiguraCuenta();
+                    break;
+                case 5:
+                    dibujarMovimientos();
+                    break;
+                default:
+                    dibujarCliente();
+            }
         } else {
             limpiar();
         }
@@ -101,7 +137,7 @@ public class pre_clientes extends Pantalla {
 
         tab_transacciones_cxc = new Tabla();
         tab_transacciones_cxc.setId("tab_transacciones_cxc");
-        tab_transacciones_cxc.setSql(ser_cliente.getSqlTransaccionesCliente(aut_clientes.getValor()));        
+        tab_transacciones_cxc.setSql(ser_cliente.getSqlTransaccionesCliente(aut_clientes.getValor()));
         tab_transacciones_cxc.getColumna("IDE_TECLB").setVisible(false);
         tab_transacciones_cxc.getColumna("IDE_CCDTR").setVisible(false);
         tab_transacciones_cxc.getColumna("NUMERO_PAGO_CCDTR").setVisible(false);
@@ -134,6 +170,114 @@ public class pre_clientes extends Pantalla {
         PanelTabla pat_panel = new PanelTabla();
         pat_panel.setPanelTabla(tab_productos);
         mep_menu.dibujar(3, "PRODUCTOS COMPRADOS POR EL CLIENTE", pat_panel);
+    }
+
+    public void dibujarConfiguraCuenta() {
+        aut_cuentas = new AutoCompletar();
+        aut_cuentas.setId("aut_cuentas");
+        aut_cuentas.setAutoCompletar(ser_contabilidad.getSqlCuentasActivos());
+        aut_cuentas.setSize(75);
+        aut_cuentas.setDisabled(true);
+        aut_cuentas.setMetodoChange("seleccionarCuentaContable");
+        if (aut_clientes.getValor() != null) {
+            aut_cuentas.setValor(ser_cliente.getCuentaCliente(aut_clientes.getValor()));
+        }
+        Grupo gru_grupo = new Grupo();
+        Grid gri_contenido = new Grid();
+        gri_contenido.setColumns(3);
+        gri_contenido.getChildren().add(new Etiqueta("CUENTA CONTABLE : "));
+        gri_contenido.getChildren().add(aut_cuentas);
+        Boton bot_cambia = new Boton();
+        bot_cambia.setValue("Cambiar");
+        bot_cambia.setIcon("ui-icon-refresh");
+        bot_cambia.setMetodo("activaCambiarCuenta");
+        gri_contenido.getChildren().add(bot_cambia);
+        gru_grupo.getChildren().add(gri_contenido);
+        gru_grupo.getChildren().add(new Etiqueta("<p style='padding-top:10px;'><strong >NOTA: </strong> La cuenta contable seleccionada se relacionará a los movimientos-transacciones que realice el Cliente, a partir que se <strong>guarde </strong> el cambio. </p>"));
+
+        mep_menu.dibujar(4, "CONFIGURACIÓN DE CUENTA CONTABLE", gru_grupo);
+    }
+
+    public void dibujarMovimientos() {
+        Grupo gru_grupo = new Grupo();
+        if (isClienteSeleccionado()) {
+            TablaGenerica tab_cuenta = ser_contabilidad.getCuenta(ser_cliente.getCuentaCliente(aut_clientes.getValor()));
+            if (!tab_cuenta.isEmpty()) {
+                gru_grupo.getChildren().add(new Etiqueta("<p style='font-size:16px;padding-bottom:5px;'> <strong>" + tab_cuenta.getValor("codig_recur_cndpc") + "</strong> &nbsp; " + tab_cuenta.getValor("nombre_cndpc") + "</p>"));
+                Fieldset fis_consulta = new Fieldset();
+                fis_consulta.setLegend("Detalle de la Consulta");
+
+                Grid gri_fechas = new Grid();
+                gri_fechas.setColumns(5);
+                gri_fechas.getChildren().add(new Etiqueta("FECHA DESDE :"));
+                cal_fecha_inicio = new Calendario();
+                cal_fecha_inicio.setValue(utilitario.getFecha(utilitario.getAnio(utilitario.getFechaActual()) + "-01-01"));
+                gri_fechas.getChildren().add(cal_fecha_inicio);
+                gri_fechas.getChildren().add(new Etiqueta("FECHA HASTA :"));
+                cal_fecha_fin = new Calendario();
+                cal_fecha_fin.setFechaActual();
+                gri_fechas.getChildren().add(cal_fecha_fin);
+                fis_consulta.getChildren().add(gri_fechas);
+
+                Boton bot_consultar = new Boton();
+                bot_consultar.setValue("Consultar");
+                bot_consultar.setIcon("ui-icon-search");
+                gri_fechas.getChildren().add(bot_consultar);
+
+                gru_grupo.getChildren().add(fis_consulta);
+
+            } else {
+                utilitario.agregarMensajeInfo("No se encontro Cuenta Contable", "El cliente seleccionado no tiene asociado una cuenta contable");
+            }
+        }
+        mep_menu.dibujar(5, "MOVIMIENTOS CONTABLES", gru_grupo);
+    }
+
+    /**
+     * *
+     * Activa el Autocompletar para cambiar la cuenta contable
+     */
+    public void activaCambiarCuenta() {
+        if (isClienteSeleccionado()) {
+            aut_cuentas.setDisabled(false);
+            utilitario.addUpdate("aut_cuentas");
+        }
+    }
+
+    /**
+     * Selecciona una nueva cuenta contable, y agrega el SQL para que pueda ser
+     * guardado
+     *
+     * @param evt
+     */
+    public void seleccionarCuentaContable(SelectEvent evt) {
+        aut_cuentas.onSelect(evt);
+        if (isClienteSeleccionado()) {
+            if (aut_cuentas.getValor() != null) {
+                utilitario.getConexion().getSqlPantalla().clear();
+                if (ser_cliente.isTieneCuentaConfiguradaCliente(aut_clientes.getValor()) == false) {
+                    //nueva
+                    utilitario.getConexion().agregarSqlPantalla(ser_cliente.getSqlInsertarCuentaCliente(aut_clientes.getValor(), aut_cuentas.getValor()));
+                } else {
+                    //modifica
+                    utilitario.getConexion().agregarSqlPantalla(ser_cliente.getSqlActualizarCuentaCliente(aut_clientes.getValor(), aut_cuentas.getValor()));
+                }
+            }
+        }
+
+    }
+
+    /**
+     * Validacion para que se seleccione un cliente del Autocompletar
+     *
+     * @return
+     */
+    private boolean isClienteSeleccionado() {
+        if (aut_clientes.getValor() == null) {
+            utilitario.agregarMensajeInfo("Debe seleccionar un Cliente", "Seleccione un cliente de la lista del Autocompletar");
+            return false;
+        }
+        return true;
     }
 
     /**
@@ -211,6 +355,10 @@ public class pre_clientes extends Pantalla {
                     utilitario.addUpdate("aut_clientes");
                 }
             }
+        } else if (mep_menu.getOpcion() == 4) {
+            if (guardarPantalla().isEmpty()) {
+                aut_cuentas.setDisabled(true);
+            }
         }
     }
 
@@ -252,6 +400,14 @@ public class pre_clientes extends Pantalla {
 
     public void setTab_productos(Tabla tab_productos) {
         this.tab_productos = tab_productos;
+    }
+
+    public AutoCompletar getAut_cuentas() {
+        return aut_cuentas;
+    }
+
+    public void setAut_cuentas(AutoCompletar aut_cuentas) {
+        this.aut_cuentas = aut_cuentas;
     }
 
 }
