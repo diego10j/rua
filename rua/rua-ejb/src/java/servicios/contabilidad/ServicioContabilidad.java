0c184450df0cacb30a3ff2381d15fe6fe8e8767e
Diego Jácome <diego10j.89@gmail.com>
2016-01-12 22:23:49 -0500

    Mejoras

--- rua/rua-ejb/src/java/servicios/contabilidad/ServicioContabilidad.java
+++ rua/rua-ejb/src/java/servicios/contabilidad/ServicioContabilidad.java
@@ -15,10 +15,10 @@ import sistema.aplicacion.Utilitario;
  */
 @Stateless
 public class ServicioContabilidad {
-    
+
     public final static String P_TIPO_CUENTA_ACTIVO = "0";
     private final Utilitario utilitario = new Utilitario();
-    
+
     public TablaGenerica getCuenta(String ide_cndpc) {
         return utilitario.consultar("SELECT * FROM con_det_plan_cuen where ide_cndpc=" + ide_cndpc);
     }
@@ -45,44 +45,18 @@ public class ServicioContabilidad {
      * @return
      */
     public String getSqlMovimientosCuenta(String ide_cndpc, String fechaInicio, String fechaFin) {
-        return "SELECT CAB.fecha_trans_cnccc as FECHA,CAB.ide_cnccc as ASIENTO,CAB.observacion_cnccc as OBSERVACION, "
-                + "LUGAR.ide_cnlap,'' as DEBE, '' as HABER, "
-                + "(DETA.valor_cndcc * sc.signo_cnscu) as valor_cndcc,'' as SALDO,PERSO.nom_geper as BENEFICIARIO, "
-                + "CUENTA.nombre_cndpc as NOMBRE_CUENTA "
+        return "SELECT CAB.ide_cnccc ,CAB.fecha_trans_cnccc as FECHA,PERSO.nom_geper as BENEFICIARIO, "
+                + "ide_cnlap,'' as DEBE, '' as HABER, "
+                + "(DETA.valor_cndcc * sc.signo_cnscu) as valor_cndcc,'' as SALDO, CAB.observacion_cnccc as OBSERVACION "
                 + "from con_cab_comp_cont CAB "
                 + "left join gen_persona PERSO on CAB.ide_geper=PERSO.ide_geper "
                 + "inner join  con_det_comp_cont DETA on CAB.ide_cnccc=DETA.ide_cnccc "
-                + "inner join con_det_plan_cuen CUENTA on  CUENTA.ide_cndpc = DETA.ide_cndpc "
-                + "inner join con_lugar_aplicac LUGAR on  LUGAR.ide_cnlap=DETA.ide_cnlap "
+                + "inner join con_det_plan_cuen CUENTA on  CUENTA.ide_cndpc = DETA.ide_cndpc "                
                 + "inner join con_tipo_cuenta tc on CUENTA.ide_cntcu=tc.ide_cntcu "
                 + "inner join con_signo_cuenta sc on tc.ide_cntcu=sc.ide_cntcu and DETA.ide_cnlap=sc.ide_cnlap "
                 + "WHERE CUENTA.ide_cndpc=" + ide_cndpc + " and fecha_trans_cnccc BETWEEN '" + fechaInicio + "' and '" + fechaFin + "' "
                 + "and ide_cneco in (" + utilitario.getVariable("p_con_estado_comp_inicial") + "," + utilitario.getVariable("p_con_estado_comprobante_normal") + "," + utilitario.getVariable("p_con_estado_comp_final") + ") "
                 + "and cab.ide_sucu=" + utilitario.getVariable("ide_sucu") + " ORDER BY CAB.fecha_trans_cnccc,cab.ide_cnccc asc";
-    }
-
-    /**
-     * Retorna los ultimos N movimientos de una cuenta de la sucursal actual
-     *
-     * @param ide_cndpc
-     * @param numMovimientos N movimientos
-     * @return
-     */
-    public String getSqlMovimientosCuenta(String ide_cndpc, int numMovimientos) {
-        return "SELECT CAB.fecha_trans_cnccc as FECHA,CAB.ide_cnccc as ASIENTO,CAB.observacion_cnccc as OBSERVACION, "
-                + "LUGAR.ide_cnlap,'' as DEBE, '' as HABER, "
-                + "(DETA.valor_cndcc * sc.signo_cnscu) as valor_cndcc,'' as SALDO,PERSO.nom_geper as BENEFICIARIO, "
-                + "CUENTA.nombre_cndpc as NOMBRE_CUENTA "
-                + "from con_cab_comp_cont CAB "
-                + "left join gen_persona PERSO on CAB.ide_geper=PERSO.ide_geper "
-                + "inner join  con_det_comp_cont DETA on CAB.ide_cnccc=DETA.ide_cnccc "
-                + "inner join con_det_plan_cuen CUENTA on  CUENTA.ide_cndpc = DETA.ide_cndpc "
-                + "inner join con_lugar_aplicac LUGAR on  LUGAR.ide_cnlap=DETA.ide_cnlap "
-                + "inner join con_tipo_cuenta tc on CUENTA.ide_cntcu=tc.ide_cntcu "
-                + "inner join con_signo_cuenta sc on tc.ide_cntcu=sc.ide_cntcu and DETA.ide_cnlap=sc.ide_cnlap "
-                + "WHERE CUENTA.ide_cndpc=" + ide_cndpc + "  "
-                + "and ide_cneco in (" + utilitario.getVariable("p_con_estado_comp_inicial") + "," + utilitario.getVariable("p_con_estado_comprobante_normal") + "," + utilitario.getVariable("p_con_estado_comp_final") + ") "
-                + "and cab.ide_sucu=" + utilitario.getVariable("ide_sucu") + " ORDER BY CAB.fecha_trans_cnccc,cab.ide_cnccc asc LIMIT " + numMovimientos;
     }
 
     /**
@@ -117,5 +91,5 @@ public class ServicioContabilidad {
         }
         return saldo;
     }
-    
+
 }
