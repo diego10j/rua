69da27dc80db8028065976e317e5a30fdd8275fa
Diego Jácome <diego10j.89@gmail.com>
2016-05-14 21:19:38 -0500

    Asiento Tipo

---
+++ rua/rua-ejb/src/java/servicios/contabilidad/ScriptConta.sql
@@ -0,0 +1,94 @@
+DROP TABLE cont_asiento_tipo;
+DROP TABLE cont_nombre_asiento_contable;
+CREATE TABLE cont_nombre_asiento_contable
+(
+  ide_conac bigint NOT NULL, -- Este campo define la clave primaria de la tabla cont_nombre_asiento_contable
+  detalle_conac character varying(250), -- detalle_conac nombre del asiento contable ejemplo: ASIENTO DE VENTAS
+  consolidado_conac boolean, -- Este campo define si se encuentre igual True  saco un solo asiento para el devengado y el pagado, se se encuentra igual a False se genera un primer asiento por devengado y un primer asiento por pagado.
+  individual_conac boolean, -- Este campo define cuando si  el campo es igual a True puedo generar asientos individuales Ej. asiento nomina permite generar asiento por empleado, si es igual a False saca un solo asiento por empleado
+  activo_conac boolean, -- Define el estado del registro si se encuentra activo o inactivo.
+  usuario_ingre character varying(50), -- Este campo define el ultimo usuario que realiza la actualizacion del registro.
+  fecha_ingre date, -- Este campo define la fecha que se realizo el ingreso por primera ves el registro.
+  hora_ingre time without time zone, -- Este campo define la ultima hora que el usuario realiza la actualizacion del registro.
+  usuario_actua character varying(50), -- Este campo define el ultimo usuario que realiza la actualizacion del registro.
+  fecha_actua date, -- Este campo define la ultima fecha que realiza la actualizacion del registro.
+  hora_actua time without time zone, -- Este campo define la hora que se realizo el ingreso por primera ves el registro.
+  ide_modu integer, -- aqui van los moidulos al que pertenece el asiento
+  CONSTRAINT pk_cont_nombre_asiento_contabl PRIMARY KEY (ide_conac),
+  CONSTRAINT fk_cont_nombre_asiento_contable_reference_gen_modulo FOREIGN KEY (ide_modu)
+      REFERENCES sis_modulo (ide_modu) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=TRUE
+);
+ALTER TABLE cont_nombre_asiento_contable OWNER TO postgres;
+COMMENT ON TABLE cont_nombre_asiento_contable IS 'Esta tabla define todos los tipos de asiento contable que se encuentran en la aplicación Ej. asiento ingreso compra, asiento pago proveedor, asiento nómina, asiento descripción, asiento tipo crédito.';
+COMMENT ON COLUMN cont_nombre_asiento_contable.ide_conac IS 'Este campo define la clave primaria de la tabla cont_nombre_asiento_contable';
+COMMENT ON COLUMN cont_nombre_asiento_contable.detalle_conac IS 'detalle_conac';
+COMMENT ON COLUMN cont_nombre_asiento_contable.consolidado_conac IS 'Este campo define si se encuentre igual True  saco un solo asiento para el devengado y el pagado, se se encuentra igual a False se genera un primer asiento por devengado y un primer asiento por pagado.  ';
+COMMENT ON COLUMN cont_nombre_asiento_contable.individual_conac IS 'Este campo define cuando si  el campo es igual a True puedo generar asientos individuales Ej. asiento nomina permite generar asiento por empleado, si es igual a False saca un solo asiento por empleado ';
+COMMENT ON COLUMN cont_nombre_asiento_contable.activo_conac IS 'Define el estado del registro si se encuentra activo o inactivo.';
+COMMENT ON COLUMN cont_nombre_asiento_contable.usuario_ingre IS 'Este campo define el ultimo usuario que realiza la actualizacion del registro.';
+COMMENT ON COLUMN cont_nombre_asiento_contable.fecha_ingre IS 'Este campo define la fecha que se realizo el ingreso por primera ves el registro.';
+COMMENT ON COLUMN cont_nombre_asiento_contable.hora_ingre IS 'Este campo define la ultima hora que el usuario realiza la actualizacion del registro.';
+COMMENT ON COLUMN cont_nombre_asiento_contable.usuario_actua IS 'Este campo define el ultimo usuario que realiza la actualizacion del registro.';
+COMMENT ON COLUMN cont_nombre_asiento_contable.fecha_actua IS 'Este campo define la ultima fecha que realiza la actualizacion del registro.';
+COMMENT ON COLUMN cont_nombre_asiento_contable.hora_actua IS 'Este campo define la hora que se realizo el ingreso por primera ves el registro.';
+
+
+CREATE TABLE cont_asiento_tipo
+(
+  ide_coast bigint NOT NULL, -- Este campo define la clave primaria de la tabla cont_tipo_asiento
+  ide_cndpc bigint, -- Este campo define la clave primaria d ela cuenta contable
+  ide_cnlap bigint, -- Este campo define la clave primaria de la tabla gen_lugar_aplica valor entero generado.
+  ide_inarti bigint, -- Articulo 
+  activo_coast boolean, -- Define el estado del registro si se encuentra activo o inactivo.
+  usuario_ingre character varying(50), -- Este campo define el ultimo usuario que realiza la actualizacion del registro.
+  fecha_ingre date, -- Este campo define la fecha que se realizo el ingreso por primera ves el registro.
+  hora_ingre time without time zone, -- Este campo define la ultima hora que el usuario realiza la actualizacion del registro.
+  usuario_actua character varying(50), -- Este campo define el ultimo usuario que realiza la actualizacion del registro.
+  fecha_actua date, -- Este campo define la ultima fecha que realiza la actualizacion del registro.
+  hora_actua time without time zone, -- Este campo define la hora que se realizo el ingreso por primera ves el registro.
+  ide_conac integer,
+  tipo_iva_coast boolean,
+  base_imponible_coast boolean,
+  valor_total_coast boolean,
+  factor_menos_coast boolean,
+  ide_prcla integer, -- este campo define la clave primaria del catalogo presupuestario
+  ide_prmop integer, -- este campo define el tipo de movieminto presupuestario.
+  CONSTRAINT pk_cont_asiento_tipo PRIMARY KEY (ide_coast),
+  CONSTRAINT fk_cont_asi_reference_cont_cat FOREIGN KEY (ide_cndpc)
+      REFERENCES con_det_plan_cuen (ide_cndpc) MATCH SIMPLE
+      ON UPDATE RESTRICT ON DELETE RESTRICT,
+  CONSTRAINT fk_cont_asi_reference_cont_nombre_asiento_contable FOREIGN KEY (ide_conac)
+      REFERENCES cont_nombre_asiento_contable (ide_conac) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT fk_cont_asi_reference_gen_luga FOREIGN KEY (ide_cnlap)
+      REFERENCES con_lugar_aplicac (ide_cnlap) MATCH SIMPLE
+      ON UPDATE RESTRICT ON DELETE RESTRICT,
+	  CONSTRAINT fk_con_inv_articulo FOREIGN KEY (ide_inarti)
+      REFERENCES inv_articulo (ide_inarti) MATCH SIMPLE
+      ON UPDATE RESTRICT ON DELETE RESTRICT,
+	  	  CONSTRAINT fk_con_pre_clasificador FOREIGN KEY (ide_prcla)
+      REFERENCES pre_clasificador (ide_prcla) MATCH SIMPLE
+      ON UPDATE RESTRICT ON DELETE RESTRICT,
+	  	  CONSTRAINT fk_con_pre_movimiento_presupuestario FOREIGN KEY (ide_prmop)
+      REFERENCES pre_movimiento_presupuestario (ide_prmop) MATCH SIMPLE
+      ON UPDATE RESTRICT ON DELETE RESTRICT
+	  
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE cont_asiento_tipo OWNER TO postgres;
+COMMENT ON TABLE cont_asiento_tipo IS 'cont_asiento_tipo';
+COMMENT ON COLUMN cont_asiento_tipo.ide_coast IS 'Este campo define la clave primaria de la tabla cont_tipo_asiento';
+COMMENT ON COLUMN cont_asiento_tipo.ide_cnlap IS 'Este campo define la clave primaria de la tabla gen_lugar_aplica valor entero generado.';
+COMMENT ON COLUMN cont_asiento_tipo.activo_coast IS 'Define el estado del registro si se encuentra activo o inactivo.';
+COMMENT ON COLUMN cont_asiento_tipo.usuario_ingre IS 'Este campo define el ultimo usuario que realiza la actualizacion del registro.';
+COMMENT ON COLUMN cont_asiento_tipo.fecha_ingre IS 'Este campo define la fecha que se realizo el ingreso por primera ves el registro.';
+COMMENT ON COLUMN cont_asiento_tipo.hora_ingre IS 'Este campo define la ultima hora que el usuario realiza la actualizacion del registro.';
+COMMENT ON COLUMN cont_asiento_tipo.usuario_actua IS 'Este campo define el ultimo usuario que realiza la actualizacion del registro.';
+COMMENT ON COLUMN cont_asiento_tipo.fecha_actua IS 'Este campo define la ultima fecha que realiza la actualizacion del registro.';
+COMMENT ON COLUMN cont_asiento_tipo.hora_actua IS 'Este campo define la hora que se realizo el ingreso por primera ves el registro.';
---
+++ rua/rua-war/src/java/paq_contabilidad/pre_asiento_tipo.java
@@ -0,0 +1,98 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paq_contabilidad;
+
+import sistema.aplicacion.Pantalla;
+import framework.componentes.Division;
+import framework.componentes.PanelTabla;
+import framework.componentes.Tabla;
+import javax.ejb.EJB;
+import servicios.contabilidad.ServicioComprobanteContabilidad;
+import servicios.contabilidad.ServicioContabilidadGeneral;
+
+/**
+ *
+ * @author Diego
+ */
+public class pre_asiento_tipo extends Pantalla {
+
+    private Tabla tab_tabla1 = new Tabla();
+    private Tabla tab_tabla2 = new Tabla();
+
+    @EJB
+    private final ServicioContabilidadGeneral ser_contabilidad = (ServicioContabilidadGeneral) utilitario.instanciarEJB(ServicioContabilidadGeneral.class);
+
+    @EJB
+    private final ServicioComprobanteContabilidad ser_comprobante = (ServicioComprobanteContabilidad) utilitario.instanciarEJB(ServicioComprobanteContabilidad.class);
+
+    public pre_asiento_tipo() {
+
+        tab_tabla1.setId("tab_tabla1");
+        tab_tabla1.setTabla("cont_nombre_asiento_contable", "ide_conac", 1);
+        tab_tabla1.getColumna("ide_modu").setCombo("sis_modulo", "ide_modu", "nom_modu", "");
+        tab_tabla1.agregarRelacion(tab_tabla2);
+        tab_tabla1.dibujar();
+        PanelTabla pat_panel1 = new PanelTabla();
+        pat_panel1.setPanelTabla(tab_tabla1);
+
+        tab_tabla2.setId("tab_tabla2");
+        tab_tabla2.setTabla("cont_asiento_tipo", "ide_coast", 2);
+        tab_tabla2.getColumna("ide_cndpc").setCombo(ser_contabilidad.getSqlCuentas());
+        tab_tabla2.getColumna("ide_cndpc").setAutoCompletar();
+        tab_tabla2.getColumna("ide_cndpc").setRequerida(true);
+        tab_tabla2.getColumna("ide_cnlap").setCombo(ser_comprobante.getSqlLugarAplica());
+        tab_tabla2.getColumna("ide_cnlap").setPermitirNullCombo(false);
+        tab_tabla2.getColumna("ide_cnlap").setMetodoChange("cambioLugarAplica");
+        tab_tabla2.getColumna("ide_cnlap").setLongitud(10);
+        tab_tabla2.getColumna("ide_inarti").setCombo("inv_articulo", "ide_inarti", "nombre_inarti", "");
+        tab_tabla2.getColumna("ide_inarti").setAutoCompletar();
+        tab_tabla2.getColumna("ide_prcla").setCombo("pre_clasificador", "ide_prcla", "codigo_clasificador_prcla,descripcion_clasificador_prcla", "");
+        tab_tabla2.getColumna("ide_prcla").setAutoCompletar();
+        tab_tabla2.getColumna("ide_prmop").setCombo("pre_movimiento_presupuestario", "ide_prmop", "detalle_prmop", "");        
+        tab_tabla2.dibujar();
+        PanelTabla pat_panel2 = new PanelTabla();
+        pat_panel2.setPanelTabla(tab_tabla2);
+        Division div_division = new Division();
+        div_division.setId("div_division");
+        div_division.dividir2(pat_panel1, pat_panel2, "50%", "H");
+        agregarComponente(div_division);
+    }
+
+    @Override
+    public void insertar() {
+        utilitario.getTablaisFocus().insertar();
+    }
+
+    @Override
+    public void guardar() {
+
+        if (tab_tabla1.guardar()) {
+            if (tab_tabla2.guardar()) {
+                guardarPantalla();
+            }
+        }
+    }
+
+    @Override
+    public void eliminar() {
+        utilitario.getTablaisFocus().eliminar();
+    }
+
+    public Tabla getTab_tabla1() {
+        return tab_tabla1;
+    }
+
+    public void setTab_tabla1(Tabla tab_tabla1) {
+        this.tab_tabla1 = tab_tabla1;
+    }
+
+    public Tabla getTab_tabla2() {
+        return tab_tabla2;
+    }
+
+    public void setTab_tabla2(Tabla tab_tabla2) {
+        this.tab_tabla2 = tab_tabla2;
+    }
+}
