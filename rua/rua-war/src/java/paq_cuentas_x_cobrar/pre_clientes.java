ce1f272b6148ed4eff969b533fd8b7eeb08dcfe1
Diego Jácome <diego10j.89@gmail.com>
2016-05-30 16:46:56 -0500

    Mejoras Conta

--- rua/rua-war/src/java/paq_cuentas_x_cobrar/pre_clientes.java
+++ rua/rua-war/src/java/paq_cuentas_x_cobrar/pre_clientes.java
@@ -76,6 +76,7 @@ public class pre_clientes extends Pantalla {
         aut_clientes.setSize(75);
         aut_clientes.setAutocompletarContenido(); // no startWith para la busqueda
         aut_clientes.setMetodoChange("seleccionarCliente");
+        aut_clientes.setGlobal(true);
         bar_botones.agregarComponente(aut_clientes);
         Boton bot_clean = new Boton();
         bot_clean.setIcon("ui-icon-cancel");
