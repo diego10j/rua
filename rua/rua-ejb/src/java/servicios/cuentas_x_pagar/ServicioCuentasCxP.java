e0452a60cef914303f1d95795a36530d8f779e94
Diego Jácome <diego10j.89@gmail.com>
2016-07-04 17:07:50 -0500

    sql CXP

--- rua/rua-ejb/src/java/servicios/cuentas_x_pagar/ServicioCuentasCxP.java
+++ rua/rua-ejb/src/java/servicios/cuentas_x_pagar/ServicioCuentasCxP.java
@@ -26,7 +26,9 @@ public class ServicioCuentasCxP extends ServicioBase {
     @PostConstruct
     public void init() {
         //Recupera todos los parametros que se van a ocupar
-        parametros = utilitario.getVariables("p_cxp_tipo_trans_factura");
+        parametros = utilitario.getVariables(
+                "p_cxp_tipo_trans_factura",
+                "p_cxp_estado_factura_normal");
 
     }
 
@@ -97,13 +99,65 @@ public class ServicioCuentasCxP extends ServicioBase {
     /**
      * Retorna los documentos cxp de una sucursal en un rango de fechas
      *
-     * @param ide_ccdaf
      * @param fechaInicio
      * @param fechaFin
      * @param tipoDocumento
      * @return
      */
     public String getSqlDocumentos(String fechaInicio, String fechaFin, String tipoDocumento) {
+        String strCondicionTipoDoc = "";
+        if (tipoDocumento != null && tipoDocumento.isEmpty() == false && tipoDocumento.equalsIgnoreCase("null") == false) {
+            strCondicionTipoDoc = " and a.ide_cntdo=" + tipoDocumento + " ";
+        }
+        return "select a.ide_cpcfa,fecha_emisi_cpcfa,nombre_cntdo, numero_cpcfa, a.ide_cpefa,nombre_cpefa ,nom_geper,identificac_geper,base_grabada_cpcfa as ventas12,base_tarifa0_cpcfa+base_no_objeto_iva_cpcfa as ventas0,valor_iva_cpcfa,total_cpcfa, observacion_cpcfa, fecha_trans_cpcfa,ide_cnccc,numero_cncre \n"
+                + " from cxp_cabece_factur a \n"
+                + " inner join gen_persona b on a.ide_geper=b.ide_geper \n"
+                + " left join cxp_estado_factur c on a.ide_cpefa=c.ide_cpefa \n"
+                + " inner join con_tipo_document e on a.ide_cntdo= e.ide_cntdo  \n"
+                + " left join con_cabece_retenc f on a.ide_cncre= f.ide_cncre  \n"
+                + " where fecha_emisi_cpcfa BETWEEN '" + fechaInicio + "' and '" + fechaFin + "' "
+                + " and a.ide_sucu=" + utilitario.getVariable("IDE_SUCU") + "\n"
+                + strCondicionTipoDoc
+                + " ORDER BY fecha_emisi_cpcfa desc,numero_cpcfa desc,ide_cpcfa desc";
+    }
+
+    /**
+     * Retorna los documentos cxp que no tienen comprobante de contabilidad de
+     * una sucursal en un rango de fechas
+     *
+     * @param fechaInicio
+     * @param fechaFin
+     * @param tipoDocumento
+     * @return
+     */
+    public String getSqlDocumentosNoContabilidad(String fechaInicio, String fechaFin, String tipoDocumento) {
+        String strCondicionTipoDoc = "";
+        if (tipoDocumento != null && tipoDocumento.isEmpty() == false && tipoDocumento.equalsIgnoreCase("null") == false) {
+            strCondicionTipoDoc = " and a.ide_cntdo=" + tipoDocumento + " ";
+        }
+        return "select a.ide_cpcfa,fecha_emisi_cpcfa,nombre_cntdo, numero_cpcfa, a.ide_cpefa,nombre_cpefa ,nom_geper,identificac_geper,base_grabada_cpcfa as ventas12,base_tarifa0_cpcfa+base_no_objeto_iva_cpcfa as ventas0,valor_iva_cpcfa,total_cpcfa, observacion_cpcfa, fecha_trans_cpcfa,numero_cncre \n"
+                + " from cxp_cabece_factur a \n"
+                + " inner join gen_persona b on a.ide_geper=b.ide_geper \n"
+                + " left join cxp_estado_factur c on a.ide_cpefa=c.ide_cpefa \n"
+                + " inner join con_tipo_document e on a.ide_cntdo= e.ide_cntdo  \n"
+                + " left join con_cabece_retenc f on a.ide_cncre= f.ide_cncre  \n"
+                + " where fecha_emisi_cpcfa BETWEEN '" + fechaInicio + "' and '" + fechaFin + "' "
+                + " and a.ide_sucu=" + utilitario.getVariable("IDE_SUCU") + "\n"
+                + " and ide_cnccc is null \n"
+                + strCondicionTipoDoc
+                + " ORDER BY fecha_emisi_cpcfa desc,numero_cpcfa desc,ide_cpcfa desc";
+    }
+
+    /**
+     * Retorna los documentos cxp que no tienen retencion de una sucursal en un
+     * rango de fechas
+     *
+     * @param fechaInicio
+     * @param fechaFin
+     * @param tipoDocumento
+     * @return
+     */
+    public String getSqlDocumentosNoRetencion(String fechaInicio, String fechaFin, String tipoDocumento) {
         String strCondicionTipoDoc = "";
         if (tipoDocumento != null && tipoDocumento.isEmpty() == false && tipoDocumento.equalsIgnoreCase("null") == false) {
             strCondicionTipoDoc = " and a.ide_cntdo=" + tipoDocumento + " ";
@@ -115,8 +169,100 @@ public class ServicioCuentasCxP extends ServicioBase {
                 + " inner join con_tipo_document e on a.ide_cntdo= e.ide_cntdo  \n"
                 + " where fecha_emisi_cpcfa BETWEEN '" + fechaInicio + "' and '" + fechaFin + "' "
                 + " and a.ide_sucu=" + utilitario.getVariable("IDE_SUCU") + "\n"
+                + " and ide_cncre is null \n"
                 + strCondicionTipoDoc
                 + " ORDER BY fecha_emisi_cpcfa desc,numero_cpcfa desc,ide_cpcfa desc";
     }
 
+    /**
+     * Retorna los documentos Anulados cxp de una sucursal en un rango de fechas
+     *
+     * @param fechaInicio
+     * @param fechaFin
+     * @param tipoDocumento
+     * @return
+     */
+    public String getSqlDocumentosAnulados(String fechaInicio, String fechaFin, String tipoDocumento) {
+        String strCondicionTipoDoc = "";
+        if (tipoDocumento != null && tipoDocumento.isEmpty() == false && tipoDocumento.equalsIgnoreCase("null") == false) {
+            strCondicionTipoDoc = " and a.ide_cntdo=" + tipoDocumento + " ";
+        }
+        return "select a.ide_cpcfa,fecha_emisi_cpcfa,nombre_cntdo, numero_cpcfa, a.ide_cpefa,nombre_cpefa ,nom_geper,identificac_geper,base_grabada_cpcfa as ventas12,base_tarifa0_cpcfa+base_no_objeto_iva_cpcfa as ventas0,valor_iva_cpcfa,total_cpcfa, observacion_cpcfa, fecha_trans_cpcfa,ide_cnccc,numero_cncre \n"
+                + " from cxp_cabece_factur a \n"
+                + " inner join gen_persona b on a.ide_geper=b.ide_geper \n"
+                + " left join cxp_estado_factur c on a.ide_cpefa=c.ide_cpefa \n"
+                + " inner join con_tipo_document e on a.ide_cntdo= e.ide_cntdo  \n"
+                + " left join con_cabece_retenc f on a.ide_cncre= f.ide_cncre  \n"
+                + " where fecha_emisi_cpcfa BETWEEN '" + fechaInicio + "' and '" + fechaFin + "' "
+                + " and a.ide_sucu=" + utilitario.getVariable("IDE_SUCU") + "\n"
+                + " and a.ide_cpefa=" + utilitario.getVariable("p_cxp_estado_factura_anulada") + " \n"
+                + strCondicionTipoDoc
+                + " ORDER BY fecha_emisi_cpcfa desc,numero_cpcfa desc,ide_cpcfa desc";
+    }
+
+    /**
+     * Retorna sentencia SQL para obtener los documentos pendientes de pago un
+     * rango de fechas por pagar X SUCURSAL
+     *
+     * @param fechaInicio
+     * @param fechaFin
+     * @param tipoDocumento
+     * @return
+     */
+    public String getSqlDocumentosPorPagar(String fechaInicio, String fechaFin, String tipoDocumento) {
+        String strCondicionTipoDoc = "";
+        if (tipoDocumento != null && tipoDocumento.isEmpty() == false && tipoDocumento.equalsIgnoreCase("null") == false) {
+            strCondicionTipoDoc = " and cf.ide_cntdo=" + tipoDocumento + " ";
+        }
+        return "select dt.ide_cpctr,"
+                + "dt.ide_cpcfa,"
+                + "case when (cf.fecha_emisi_cpcfa) is null then ct.fecha_trans_cpctr else cf.fecha_emisi_cpcfa end as FECHA,"
+                + "nombre_cntdo,cf.numero_cpcfa,"
+                + "cf.total_cpcfa,"
+                + "sum (dt.valor_cpdtr*tt.signo_cpttr) as saldo_x_pagar,"
+                //  + "sum (dt.valor_cpdtr*tt.signo_cpttr)-sum (dt.valor_anticipo_cpdtr) as saldo_x_pagar, "  ----ESTO ESTABA ORIGINALMENTE
+                + "case when (cf.observacion_cpcfa) is NULL then ct.observacion_cpctr else cf.observacion_cpcfa end as OBSERVACION "
+                + "from cxp_detall_transa dt "
+                + "left join cxp_cabece_transa ct on dt.ide_cpctr=ct.ide_cpctr "
+                + "left join cxp_cabece_factur cf on cf.ide_cpcfa=ct.ide_cpcfa and cf.ide_cpefa=" + utilitario.getVariable("p_cxp_estado_factura_normal") + " "
+                + "left join cxp_tipo_transacc tt on tt.ide_cpttr=dt.ide_cpttr "
+                + "inner join con_tipo_document co on cf.ide_cntdo= co.ide_cntdo "
+                + "where cf.ide_sucu=" + utilitario.getVariable("ide_sucu") + " "
+                + "and cf.fecha_emisi_cpcfa BETWEEN '" + fechaInicio + "' and '" + fechaFin + "' "
+                + strCondicionTipoDoc
+                + "GROUP BY dt.ide_cpcfa,dt.ide_cpctr,cf.numero_cpcfa,nombre_cntdo, "
+                + "cf.observacion_cpcfa,ct.observacion_cpctr,cf.fecha_emisi_cpcfa,ct.fecha_trans_cpctr,cf.total_cpcfa "
+                + "HAVING sum (dt.valor_cpdtr*tt.signo_cpttr)-sum (dt.valor_anticipo_cpdtr) > 0 "
+                + "ORDER BY cf.fecha_emisi_cpcfa ASC ,ct.fecha_trans_cpctr ASC,dt.ide_cpctr ASC";
+    }
+
+    /**
+     * Sql de total de compras por mes por sucursal
+     *
+     * @param anio
+     * @return
+     */
+    public String getSqlTotalComprasMensuales(String anio) {
+
+        return "select nombre_gemes,\n"
+                + "(select count(ide_cpcfa) as num_documentos from cxp_cabece_factur a where EXTRACT(MONTH FROM fecha_emisi_cpcfa)=ide_gemes and EXTRACT(YEAR FROM fecha_emisi_cpcfa) in(" + anio + ") and ide_cpefa=" + parametros.get("p_cxp_estado_factura_normal") + " and ide_sucu=" + utilitario.getVariable("IDE_SUCU") + "),\n"
+                + "(select sum(base_grabada_cpcfa) as compras12 from cxp_cabece_factur a where EXTRACT(MONTH FROM fecha_emisi_cpcfa)=ide_gemes and EXTRACT(YEAR FROM fecha_emisi_cpcfa) in(" + anio + ")  and ide_cpefa=" + parametros.get("p_cxp_estado_factura_normal") + " and ide_sucu=" + utilitario.getVariable("IDE_SUCU") + "),\n"
+                + "(select sum(base_tarifa0_cpcfa+base_no_objeto_iva_cpcfa) as compras0 from cxp_cabece_factur a where EXTRACT(MONTH FROM fecha_emisi_cpcfa)=ide_gemes and EXTRACT(YEAR FROM fecha_emisi_cpcfa) in(" + anio + ")  and ide_cpefa=" + parametros.get("p_cxp_estado_factura_normal") + " and ide_sucu=" + utilitario.getVariable("IDE_SUCU") + "),\n"
+                + "(select sum(valor_iva_cpcfa) as iva from cxp_cabece_factur a where EXTRACT(MONTH FROM fecha_emisi_cpcfa)=ide_gemes and EXTRACT(YEAR FROM fecha_emisi_cpcfa) in(" + anio + ")   and ide_cpefa=" + parametros.get("p_cxp_estado_factura_normal") + " and ide_sucu=" + utilitario.getVariable("IDE_SUCU") + "),\n"
+                + "(select sum(total_cpcfa) as total from cxp_cabece_factur a where EXTRACT(MONTH FROM fecha_emisi_cpcfa)=ide_gemes and EXTRACT(YEAR FROM fecha_emisi_cpcfa) in(" + anio + ")  and ide_cpefa=" + parametros.get("p_cxp_estado_factura_normal") + " and ide_sucu=" + utilitario.getVariable("IDE_SUCU") + ") \n"
+                + "from gen_mes \n"
+                + "order by ide_gemes";
+    }
+
+    /**
+     * Retorna el sql con los anios que exuste facturacion en la empresa
+     *
+     * @return
+     */
+    public String getSqlAniosFacturacion() {
+        return "select distinct EXTRACT(YEAR FROM fecha_emisi_cpcfa)||'' as anio,EXTRACT(YEAR FROM fecha_emisi_cpcfa)||'' as nom_anio  "
+                + "from cxp_cabece_factur where ide_empr=" + utilitario.getVariable("IDE_EMPR") + " order by 1 desc ";
+    }
+
+    
 }
--- rua/rua-war/src/java/paq_cuentas_x_pagar/pre_documentosCxP.java
+++ rua/rua-war/src/java/paq_cuentas_x_pagar/pre_documentosCxP.java
@@ -10,10 +10,13 @@ import framework.componentes.Boton;
 import framework.componentes.Calendario;
 import framework.componentes.Combo;
 import framework.componentes.Etiqueta;
+import framework.componentes.Grid;
 import framework.componentes.Grupo;
 import framework.componentes.MenuPanel;
 import framework.componentes.PanelTabla;
 import framework.componentes.Tabla;
+import framework.componentes.graficos.GraficoCartesiano;
+import framework.componentes.graficos.GraficoPastel;
 import javax.ejb.EJB;
 import servicios.cuentas_x_pagar.ServicioCuentasCxP;
 import sistema.aplicacion.Pantalla;
@@ -33,6 +36,9 @@ public class pre_documentosCxP extends Pantalla {
     private final ServicioCuentasCxP ser_cuentas_cxp = (ServicioCuentasCxP) utilitario.instanciarEJB(ServicioCuentasCxP.class);
 
     private Tabla tab_tabla1 = new Tabla();
+    private Combo com_periodo;
+    private GraficoCartesiano gca_facturas;
+    private GraficoPastel gpa_facturas;
 
     public pre_documentosCxP() {
         bar_botones.quitarBotonsNavegacion();
@@ -40,7 +46,7 @@ public class pre_documentosCxP extends Pantalla {
         bar_botones.quitarBotonEliminar();
 
         com_tipo_documento.setCombo(ser_cuentas_cxp.getSqlTipoDocumentosCxP());
-        com_tipo_documento.setMetodo("filrarTipoDocumento");
+        com_tipo_documento.setMetodo("actualizarFiltros");
 
         bar_botones.agregarComponente(new Etiqueta("TIPO DE DOCUMENTO :"));
         bar_botones.agregarComponente(com_tipo_documento);
@@ -55,7 +61,7 @@ public class pre_documentosCxP extends Pantalla {
 
         Boton bot_consultar = new Boton();
         bot_consultar.setTitle("Buscar");
-        bot_consultar.setMetodo("actualizarFacturas");
+        bot_consultar.setMetodo("actualizarFiltros");
         bot_consultar.setIcon("ui-icon-search");
         bar_botones.agregarComponente(bot_consultar);
 
@@ -70,7 +76,7 @@ public class pre_documentosCxP extends Pantalla {
         mep_menu.agregarItem("Documentos Anulados", "dibujarDocumentosAnulados", "ui-icon-cancel");
         mep_menu.agregarItem("Documentos Por Pagar", "dibujarDocumentosPorPagar", "ui-icon-calculator");
         mep_menu.agregarSubMenu("INFORMES");
-        mep_menu.agregarItem("Grafico de Compras", "dibujarGraficoVentas", "ui-icon-clock");
+        mep_menu.agregarItem("Grafico de Compras", "dibujarGraficoCompras", "ui-icon-clock");
         // mep_menu.agregarItem("Estadística de Ventas", "dibujarEstadisticas", "ui-icon-bookmark");        
         mep_menu.agregarSubMenu("FACTURAS ELECTRONICAS");
         mep_menu.agregarItem("Ingresar Factura ", "dibujarFacturaElectronica", "ui-icon-signal-diag");
@@ -95,11 +101,11 @@ public class pre_documentosCxP extends Pantalla {
         tab_tabla1.getColumna("valor_iva_cpcfa").alinearDerecha();
         tab_tabla1.getColumna("total_cpcfa").alinearDerecha();
         tab_tabla1.getColumna("total_cpcfa").setEstilo("font-size: 12px;font-weight: bold;");
-        tab_tabla1.setRows(15);
+        tab_tabla1.setRows(20);
         tab_tabla1.setLectura(true);
         //COLOR VERDE FACTURAS NO CONTABILIZADAS
         //COLOR ROJO FACTURAS ANULADAS
-        tab_tabla1.setValueExpression("rowStyleClass", "fila.campos[2] eq '" + utilitario.getVariable("p_cxc_estado_factura_anulada") + "' ? 'text-red' : fila.campos[13] eq null  ? 'text-green' : null");
+        tab_tabla1.setValueExpression("rowStyleClass", "fila.campos[5] eq '" + utilitario.getVariable("p_cxp_estado_factura_anulada") + "' ? 'text-red' : fila.campos[18] eq null  ? 'text-green' : null");
         tab_tabla1.dibujar();
         PanelTabla pat_panel = new PanelTabla();
         pat_panel.setPanelTabla(tab_tabla1);
@@ -109,32 +115,193 @@ public class pre_documentosCxP extends Pantalla {
     }
 
     public void dibujarDocumentosNoContabilizadas() {
-
+        tab_tabla1 = new Tabla();
+        tab_tabla1.setId("tab_tabla1");
+        tab_tabla1.setSql(ser_cuentas_cxp.getSqlDocumentosNoContabilidad(cal_fecha_inicio.getFecha(), cal_fecha_fin.getFecha(), String.valueOf(com_tipo_documento.getValue())));
+        tab_tabla1.setCampoPrimaria("ide_cpcfa");
+        tab_tabla1.getColumna("ide_cpcfa").setVisible(false);
+        tab_tabla1.getColumna("ide_cpefa").setVisible(false);
+        tab_tabla1.getColumna("nombre_cpefa").setFiltroContenido();
+        tab_tabla1.getColumna("numero_cpcfa").setFiltroContenido();
+        tab_tabla1.getColumna("nom_geper").setFiltroContenido();
+        tab_tabla1.getColumna("identificac_geper").setFiltroContenido();
+        tab_tabla1.getColumna("ide_cnccc").setFiltroContenido();
+        tab_tabla1.getColumna("ventas0").alinearDerecha();
+        tab_tabla1.getColumna("ventas12").alinearDerecha();
+        tab_tabla1.getColumna("valor_iva_cpcfa").alinearDerecha();
+        tab_tabla1.getColumna("total_cpcfa").alinearDerecha();
+        tab_tabla1.getColumna("total_cpcfa").setEstilo("font-size: 12px;font-weight: bold;");
+        tab_tabla1.setRows(20);
+        tab_tabla1.setLectura(true);
+        tab_tabla1.dibujar();
+        PanelTabla pat_panel = new PanelTabla();
+        pat_panel.setPanelTabla(tab_tabla1);
+        Grupo gru = new Grupo();
+        gru.getChildren().add(pat_panel);
+        mep_menu.dibujar(2, "DOCUMENTOS POR PAGAR SIN COMPROBANTE CONTABLE", gru);
     }
 
     public void dibujarDocumentosNoRetencion() {
-
+        tab_tabla1 = new Tabla();
+        tab_tabla1.setId("tab_tabla1");
+        tab_tabla1.setSql(ser_cuentas_cxp.getSqlDocumentosNoRetencion(cal_fecha_inicio.getFecha(), cal_fecha_fin.getFecha(), String.valueOf(com_tipo_documento.getValue())));
+        tab_tabla1.setCampoPrimaria("ide_cpcfa");
+        tab_tabla1.getColumna("ide_cpcfa").setVisible(false);
+        tab_tabla1.getColumna("ide_cpefa").setVisible(false);
+        tab_tabla1.getColumna("nombre_cpefa").setFiltroContenido();
+        tab_tabla1.getColumna("numero_cpcfa").setFiltroContenido();
+        tab_tabla1.getColumna("nom_geper").setFiltroContenido();
+        tab_tabla1.getColumna("identificac_geper").setFiltroContenido();
+        tab_tabla1.getColumna("ide_cnccc").setFiltroContenido();
+        tab_tabla1.getColumna("ventas0").alinearDerecha();
+        tab_tabla1.getColumna("ventas12").alinearDerecha();
+        tab_tabla1.getColumna("valor_iva_cpcfa").alinearDerecha();
+        tab_tabla1.getColumna("total_cpcfa").alinearDerecha();
+        tab_tabla1.getColumna("total_cpcfa").setEstilo("font-size: 12px;font-weight: bold;");
+        tab_tabla1.setRows(20);
+        tab_tabla1.setLectura(true);
+        tab_tabla1.dibujar();
+        PanelTabla pat_panel = new PanelTabla();
+        pat_panel.setPanelTabla(tab_tabla1);
+        Grupo gru = new Grupo();
+        gru.getChildren().add(pat_panel);
+        mep_menu.dibujar(3, "DOCUMENTOS POR PAGAR SIN COMPROBANTE DE RETENCION", gru);
     }
 
     public void dibujarDocumentosAnulados() {
-
+        tab_tabla1 = new Tabla();
+        tab_tabla1.setId("tab_tabla1");
+        tab_tabla1.setSql(ser_cuentas_cxp.getSqlDocumentosAnulados(cal_fecha_inicio.getFecha(), cal_fecha_fin.getFecha(), String.valueOf(com_tipo_documento.getValue())));
+        tab_tabla1.setCampoPrimaria("ide_cpcfa");
+        tab_tabla1.getColumna("ide_cpcfa").setVisible(false);
+        tab_tabla1.getColumna("ide_cpefa").setVisible(false);
+        tab_tabla1.getColumna("nombre_cpefa").setFiltroContenido();
+        tab_tabla1.getColumna("numero_cpcfa").setFiltroContenido();
+        tab_tabla1.getColumna("nom_geper").setFiltroContenido();
+        tab_tabla1.getColumna("identificac_geper").setFiltroContenido();
+        tab_tabla1.getColumna("ide_cnccc").setFiltroContenido();
+        tab_tabla1.getColumna("ventas0").alinearDerecha();
+        tab_tabla1.getColumna("ventas12").alinearDerecha();
+        tab_tabla1.getColumna("valor_iva_cpcfa").alinearDerecha();
+        tab_tabla1.getColumna("total_cpcfa").alinearDerecha();
+        tab_tabla1.getColumna("total_cpcfa").setEstilo("font-size: 12px;font-weight: bold;");
+        tab_tabla1.setRows(20);
+        tab_tabla1.setLectura(true);
+        tab_tabla1.dibujar();
+        PanelTabla pat_panel = new PanelTabla();
+        pat_panel.setPanelTabla(tab_tabla1);
+        Grupo gru = new Grupo();
+        gru.getChildren().add(pat_panel);
+        mep_menu.dibujar(4, "DOCUMENTOS POR PAGAR ANULADOS", gru);
     }
 
     public void dibujarDocumentosPorPagar() {
-
+        tab_tabla1 = new Tabla();
+        tab_tabla1.setId("tab_tabla1");
+        tab_tabla1.setId("tab_facturas");
+        tab_tabla1.setSql(ser_cuentas_cxp.getSqlDocumentosPorPagar(cal_fecha_inicio.getFecha(), cal_fecha_fin.getFecha(), String.valueOf(com_tipo_documento.getValue())));
+        tab_tabla1.getColumna("saldo_x_pagar").setEstilo("font-size: 13px;font-weight: bold");
+        tab_tabla1.getColumna("saldo_x_pagar").alinearDerecha();
+        tab_tabla1.setCampoPrimaria("ide_cpctr");
+        tab_tabla1.getColumna("ide_cpctr").setVisible(false);
+        tab_tabla1.getColumna("ide_cpcfa").setVisible(false);
+        tab_tabla1.getColumna("fecha").setVisible(true);
+        tab_tabla1.getColumna("numero_cpcfa").setNombreVisual("N. FACTURA");
+        tab_tabla1.getColumna("numero_cpcfa").setFiltroContenido();
+        tab_tabla1.getColumna("saldo_x_pagar").setNombreVisual("SALDO");
+        tab_tabla1.getColumna("total_cpcfa").setNombreVisual("TOTAL");
+        tab_tabla1.getColumna("total_cpcfa").setEstilo("font-size: 13px;");
+        tab_tabla1.getColumna("total_cpcfa").alinearDerecha();
+        tab_tabla1.setLectura(true);
+        tab_tabla1.setColumnaSuma("saldo_x_pagar");
+        tab_tabla1.dibujar();
+        PanelTabla pat_panel = new PanelTabla();
+        pat_panel.setPanelTabla(tab_tabla1);
+        Grupo gru = new Grupo();
+        gru.getChildren().add(pat_panel);
+        mep_menu.dibujar(5, "DOCUMENTOS PENDIENTES DE PAGO", gru);
     }
 
-    public void dibujarGraficoVentas() {
+    public void dibujarGraficoCompras() {
+        Grupo grupo = new Grupo();
+        gca_facturas = new GraficoCartesiano();
+        gca_facturas.setId("gca_facturas");
 
+        gpa_facturas = new GraficoPastel();
+        gpa_facturas.setId("gpa_facturas");
+        gpa_facturas.setShowDataLabels(true);
+        gpa_facturas.setStyle("width:300px;");
+
+        com_periodo = new Combo();
+        com_periodo.setMetodo("actualizarFacturas");
+        com_periodo.setCombo(ser_cuentas_cxp.getSqlAniosFacturacion());
+        com_periodo.eliminarVacio();
+        com_periodo.setValue(utilitario.getAnio(utilitario.getFechaActual()));
+
+        tab_tabla1 = new Tabla();
+        tab_tabla1.setId("tab_tabla1");
+        tab_tabla1.setSql(ser_cuentas_cxp.getSqlTotalComprasMensuales(String.valueOf(com_periodo.getValue())));
+        tab_tabla1.setLectura(true);
+        tab_tabla1.setColumnaSuma("num_facturas,ventas12,ventas0,iva,total");
+        tab_tabla1.getColumna("num_documentos").alinearDerecha();
+        tab_tabla1.getColumna("compras12").alinearDerecha();
+        tab_tabla1.getColumna("compras0").alinearDerecha();
+        tab_tabla1.getColumna("iva").alinearDerecha();
+        tab_tabla1.getColumna("total").alinearDerecha();
+        tab_tabla1.dibujar();
+
+        Grid gri_opciones = new Grid();
+        gri_opciones.setColumns(2);
+        gri_opciones.getChildren().add(new Etiqueta("<strong>PERÍODO :</strong>"));
+        gri_opciones.getChildren().add(com_periodo);
+        PanelTabla pat_panel = new PanelTabla();
+        pat_panel.getChildren().add(gri_opciones);
+        pat_panel.setPanelTabla(tab_tabla1);
+
+        Grid gri = new Grid();
+        gri.setWidth("100%");
+        gri.setColumns(2);
+        gpa_facturas.agregarSerie(tab_tabla1, "nombre_gemes", "num_documentos");
+        gri.getChildren().add(pat_panel);
+        gri.getChildren().add(gpa_facturas);
+        grupo.getChildren().add(gri);
+
+        gca_facturas.setTitulo("COMPRAS MENSUALES");
+        gca_facturas.agregarSerie(tab_tabla1, "nombre_gemes", "total", "VENTAS " + String.valueOf(com_periodo.getValue()));
+        grupo.getChildren().add(gca_facturas);
+        mep_menu.dibujar(5, "GRAFICOS DE COMPRAS", grupo);
     }
 
     public void dibujarFacturaElectronica() {
 
     }
 
-    public void filrarTipoDocumento() {
-        tab_tabla1.setSql(ser_cuentas_cxp.getSqlDocumentos(cal_fecha_inicio.getFecha(), cal_fecha_fin.getFecha(), String.valueOf(com_tipo_documento.getValue())));
-        tab_tabla1.ejecutarSql();
+    public void actualizarFiltros() {
+        switch (mep_menu.getOpcion()) {
+            case 1:
+                tab_tabla1.setSql(ser_cuentas_cxp.getSqlDocumentos(cal_fecha_inicio.getFecha(), cal_fecha_fin.getFecha(), String.valueOf(com_tipo_documento.getValue())));
+                tab_tabla1.ejecutarSql();
+                break;
+            case 2:
+                tab_tabla1.setSql(ser_cuentas_cxp.getSqlDocumentosNoContabilidad(cal_fecha_inicio.getFecha(), cal_fecha_fin.getFecha(), String.valueOf(com_tipo_documento.getValue())));
+                tab_tabla1.ejecutarSql();
+                break;
+            case 3:
+                tab_tabla1.setSql(ser_cuentas_cxp.getSqlDocumentosNoRetencion(cal_fecha_inicio.getFecha(), cal_fecha_fin.getFecha(), String.valueOf(com_tipo_documento.getValue())));
+                tab_tabla1.ejecutarSql();
+                break;
+            case 4:
+                tab_tabla1.setSql(ser_cuentas_cxp.getSqlDocumentosAnulados(cal_fecha_inicio.getFecha(), cal_fecha_fin.getFecha(), String.valueOf(com_tipo_documento.getValue())));
+                tab_tabla1.ejecutarSql();
+                break;
+            case 5:
+                tab_tabla1.setSql(ser_cuentas_cxp.getSqlDocumentosPorPagar(cal_fecha_inicio.getFecha(), cal_fecha_fin.getFecha(), String.valueOf(com_tipo_documento.getValue())));
+                tab_tabla1.ejecutarSql();
+                break;
+            default:
+                break;
+        }
+
     }
 
     @Override
