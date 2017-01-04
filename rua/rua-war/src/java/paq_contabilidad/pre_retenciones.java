20e586e5ab3f28065918b4349e37433cc4825233
Diego Jácome <diego10j.89@gmail.com>
2017-01-04 16:12:07 -0500

    Mejoras 1

--- rua/rua-war/src/java/paq_contabilidad/pre_retenciones.java
+++ rua/rua-war/src/java/paq_contabilidad/pre_retenciones.java
@@ -28,7 +28,7 @@ import sistema.aplicacion.Pantalla;
  * @author djacome
  */
 public class pre_retenciones extends Pantalla {
-    
+
     private final MenuPanel mep_menu = new MenuPanel();
     private final Combo com_autoriza = new Combo();
     private final Calendario cal_fecha_inicio = new Calendario();
@@ -36,40 +36,40 @@ public class pre_retenciones extends Pantalla {
     private Combo com_impuesto;
     @EJB
     private final ServicioRetenciones ser_retencion = (ServicioRetenciones) utilitario.instanciarEJB(ServicioRetenciones.class);
-    
+
     private Tabla tab_tabla;
-    
+
     private Reporte rep_reporte = new Reporte();
     private SeleccionFormatoReporte sel_rep = new SeleccionFormatoReporte();
-    
+
     public pre_retenciones() {
         bar_botones.quitarBotonsNavegacion();
         bar_botones.quitarBotonInsertar();
         bar_botones.quitarBotonGuardar();
         bar_botones.quitarBotonEliminar();
         bar_botones.agregarReporte();
-        
+
         com_autoriza.setCombo(ser_retencion.getSqlComboAutorizaciones());
         com_autoriza.setMetodo("actualizarConsulta");
         bar_botones.agregarComponente(new Etiqueta("NUM. AUTORIZACIÓN: "));
         bar_botones.agregarComponente(com_autoriza);
-        
+
         bar_botones.agregarSeparador();
         bar_botones.agregarComponente(new Etiqueta("FECHA DESDE :"));
-        
+
         cal_fecha_inicio.setValue(utilitario.getFecha(utilitario.getAnio(utilitario.getFechaActual()) + "-01-01"));
         bar_botones.agregarComponente(cal_fecha_inicio);
         bar_botones.agregarComponente(new Etiqueta("FECHA HASTA :"));
-        
+
         cal_fecha_fin.setFechaActual();
         bar_botones.agregarComponente(cal_fecha_fin);
-        
+
         Boton bot_consultar = new Boton();
         bot_consultar.setMetodo("actualizarConsulta");
         bot_consultar.setIcon("ui-icon-search");
-        
+
         bar_botones.agregarBoton(bot_consultar);
-        
+
         mep_menu.setMenuPanel("COMPROBANTES DE RETENCIÓN", "23%");
         mep_menu.agregarItem("Listado de Retenciones", "dibujarListado", "ui-icon-bookmark"); //1
         mep_menu.agregarItem("Retenciones Anuladas", "dibujarAnuladas", "ui-icon-bookmark");//2
@@ -78,19 +78,19 @@ public class pre_retenciones extends Pantalla {
         mep_menu.agregarItem("Listado de RetencionesVentas", "dibujarListadoVentas", "ui-icon-bookmark");//3
         agregarComponente(mep_menu);
         dibujarListado();
-        
+
         rep_reporte.setId("rep_reporte");
         rep_reporte.getBot_aceptar().setMetodo("aceptarReporte");
         sel_rep.setId("sel_rep");
         agregarComponente(rep_reporte);
         agregarComponente(sel_rep);
     }
-    
+
     public void dibujarConsulta() {
         Grupo gru_grupo = new Grupo();
         com_impuesto = new Combo();
         com_impuesto.setCombo("SELECT ide_cncim, nombre_cncim,casillero_cncim FROM con_cabece_impues");
-        
+
         tab_tabla = new Tabla();
         tab_tabla.setId("tab_tabla");
         tab_tabla.setSql(ser_retencion.getSqlConsultaImpuestos(String.valueOf(com_autoriza.getValue()), cal_fecha_inicio.getFecha(), cal_fecha_fin.getFecha(), String.valueOf(com_impuesto.getValue())));
@@ -100,7 +100,7 @@ public class pre_retenciones extends Pantalla {
         tab_tabla.setRows(25);
         tab_tabla.dibujar();
         PanelTabla pat_panel = new PanelTabla();
-        
+
         Grid g1 = new Grid();
         g1.setColumns(3);
         g1.getChildren().add(new Etiqueta("IMPUESTO"));
@@ -108,40 +108,41 @@ public class pre_retenciones extends Pantalla {
         btbus.setValue("Consultar");
         btbus.setIcon("ui-icon-search");
         btbus.setMetodo("actualizarConsultar");
-        
+
         g1.getChildren().add(com_impuesto);
         g1.getChildren().add(btbus);
         pat_panel.getChildren().add(g1);
         pat_panel.setPanelTabla(tab_tabla);
-        
+
         gru_grupo.getChildren().add(pat_panel);
-        
+
         mep_menu.dibujar(4, "CONSULTAR POR IMPUESTO", gru_grupo);
     }
-    
+
     public void actualizarConsultar() {
-        
+        tab_tabla.setSql(ser_retencion.getSqlConsultaImpuestos(String.valueOf(com_autoriza.getValue()), cal_fecha_inicio.getFecha(), cal_fecha_fin.getFecha(), String.valueOf(com_impuesto.getValue())));
+        tab_tabla.ejecutarSql();
     }
-    
+
     public void dibujarListadoVentas() {
         Grupo gru_grupo = new Grupo();
         mep_menu.dibujar(3, "COMPROBANTES DE RETENCIÓN EN VENTAS", gru_grupo);
     }
-    
+
     public void dibujarListado() {
         Grupo gru_grupo = new Grupo();
-        
+
         Barra bar_menu = new Barra();
         bar_menu.setId("bar_menu");
         bar_menu.limpiar();
-        
+
         Boton bot_anular = new Boton();
         bot_anular.setValue("Anular Comprobante de Retención");
         bot_anular.setMetodo("anularRetencion");
         bar_menu.agregarComponente(bot_anular);
-        
+
         gru_grupo.getChildren().add(bar_menu);
-        
+
         tab_tabla = new Tabla();
         tab_tabla.setId("tab_tabla");
         tab_tabla.setSql(ser_retencion.getSqlRetenciones(String.valueOf(com_autoriza.getValue()), cal_fecha_inicio.getFecha(), cal_fecha_fin.getFecha()));
@@ -154,7 +155,7 @@ public class pre_retenciones extends Pantalla {
         gru_grupo.getChildren().add(pat_panel);
         mep_menu.dibujar(1, "COMPROBANTES DE RETENCIÓN", gru_grupo);
     }
-    
+
     public void anularRetencion() {
         if (tab_tabla.getValorSeleccionado() != null) {
             ser_retencion.anularComprobanteRetencion(tab_tabla.getValorSeleccionado());
@@ -163,7 +164,7 @@ public class pre_retenciones extends Pantalla {
             utilitario.agregarMensajeInfo("Seleccione un Comprobante de Retención", "");
         }
     }
-    
+
     public void actualizarConsulta() {
         if (mep_menu.getOpcion() == 1) {
             tab_tabla.setSql(ser_retencion.getSqlRetenciones(String.valueOf(com_autoriza.getValue()), cal_fecha_inicio.getFecha(), cal_fecha_fin.getFecha()));
@@ -173,13 +174,13 @@ public class pre_retenciones extends Pantalla {
             tab_tabla.ejecutarSql();
         }
     }
-    
+
     @Override
     public void abrirListaReportes() {
         rep_reporte.dibujar();
     }
     Map parametro = new HashMap();
-    
+
     @Override
     public void aceptarReporte() {
         if (rep_reporte.getReporteSelecionado().equals("Comprobante de Retención")) {
@@ -194,11 +195,11 @@ public class pre_retenciones extends Pantalla {
                 } else {
                     utilitario.agregarMensajeInfo("Seleccione un Comprobante de Retención", "");
                 }
-                
+
             }
         }
     }
-    
+
     public void dibujarAnuladas() {
         Grupo gru_grupo = new Grupo();
         tab_tabla = new Tabla();
@@ -212,43 +213,43 @@ public class pre_retenciones extends Pantalla {
         gru_grupo.getChildren().add(pat_panel);
         mep_menu.dibujar(2, "COMPROBANTES DE RETENCIÓN ANULADOS", gru_grupo);
     }
-    
+
     @Override
     public void insertar() {
-        
+
     }
-    
+
     @Override
     public void guardar() {
-        
+
     }
-    
+
     @Override
     public void eliminar() {
     }
-    
+
     public Tabla getTab_tabla() {
         return tab_tabla;
     }
-    
+
     public void setTab_tabla(Tabla tab_tabla) {
         this.tab_tabla = tab_tabla;
     }
-    
+
     public Reporte getRep_reporte() {
         return rep_reporte;
     }
-    
+
     public void setRep_reporte(Reporte rep_reporte) {
         this.rep_reporte = rep_reporte;
     }
-    
+
     public SeleccionFormatoReporte getSel_rep() {
         return sel_rep;
     }
-    
+
     public void setSel_rep(SeleccionFormatoReporte sel_rep) {
         this.sel_rep = sel_rep;
     }
-    
+
 }
