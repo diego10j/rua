e03ed136b2ae918549ffc9ffeb47e8608225fe2c
hhslouis <hhsoul_louis@hotmail.com>
2018-05-21 22:34:42 -0500

    presupuesto andres r

--- rua/rua-ejb/src/java/paq_adquisicion/ejb/ServiciosAdquisiones.java
+++ rua/rua-ejb/src/java/paq_adquisicion/ejb/ServiciosAdquisiones.java
@@ -96,6 +96,40 @@ public class ServiciosAdquisiones {
         sql = "SELECT IDE_ADEMPLE, CEDULA_ADEMPLE, NOMBRES_ADEMPLE FROM ADQ_EMPLEADO ORDER BY NOMBRES_ADEMPLE";
         return sql;
     }
+    
+    public String getDatosEmpleadoCodigo(String ide_usua) {
+        String sql = "";
+        sql = "select ide_ademple, cedula_ademple, nombres_ademple from adq_empleado where ide_usua in (select ide_usua from sis_usuario where ide_usua = "+ide_usua+")";
+        return sql;
+    }
+    public String getDatosFactura() {
+        String sql = "";
+        sql = "select ide_ccdaf, serie_ccdaf, autorizacion_ccdaf, observacion_ccdaf from cxc_datos_fac";
+        return sql;
+    }
+    
+    public String getTipoCaja() {
+        String sql = "";
+        sql = "select ide_cocaj, detalle_cocaj from cont_caja order by detalle_cocaj";
+        return sql;
+    }
+    
+    public String getCajas(String ide_emple, String activo) {
+        String sql = "";
+        sql = " select ide_cxcaem, detalle_cocaj from cxc_caja_empleado a\n" +
+              " inner join cont_caja b on a.ide_cocaj = b.ide_cocaj\n" +
+              " where a.ide_ademple = "+ide_emple+"\n" +
+              " and activo_cxcaem = "+activo+" ";
+        return sql;
+    }
+    public String getPuntoEmisionAdquisiciones(String ide_emple, String activo) {
+        String sql = "";
+        sql = " select a.ide_cxdaem, b.serie_ccdaf, b.autorizacion_ccdaf, b.observacion_ccdaf from cxc_dat_fac_emp a\n" +
+              " inner join cxc_datos_fac b on a.ide_ccdaf = b.ide_ccdaf\n" +
+              " where a.ide_ademple = "+ide_emple+"\n" +
+              " and activo_cxdaem = "+activo+" ";
+        return sql;
+    }
 
     public String getEmpleadoDepartamento(String tipo, String empleado, String estado, String departamento) {
         String sql = "select ide_ademde,CEDULA_ADEMPLE,NOMBRES_ADEMPLE from ADQ_EMPLEADO_DEPARTAMENTO a,ADQ_EMPLEADO b where a.IDE_ADEMPLE = b.IDE_ADEMPLE ";
@@ -180,6 +214,57 @@ public class ServiciosAdquisiones {
              "from gth_empleado order by nombres_empleado";
                 return sql;
     }
+    
+    public String getDatosProveedor (){
+        String sql = "";
+        sql= "select ide_geper, identificac_geper, nom_geper from gen_persona order by nom_geper";
+                return sql;
+    }
+    
+    public String getDatosPresupuesto (){
+        String sql = "";
+        sql= "select c.ide_prpot,comprometido_prpot,comprometido_prpot  - (case when devengado is null then 0 else devengado end) as saldoxdevengado,fecha_tramite_prtra,cod_programa_prpro,codigo_clasificador_prcla,detalle_actividad,detalle_subactividad,detalle_prfuf,elaborado,observaciones_prtra,a.ide_geedp,empleado_responsable \n" +
+"            from pre_tramite a \n" +
+"            left join \n" +
+"            ( \n" +
+"            select apellido_paterno_gtemp||' '||(case when apellido_materno_gtemp is null then '' else apellido_materno_gtemp end)||' '||primer_nombre_gtemp ||' '|| \n" +
+"            (case when segundo_nombre_gtemp is null then '' else segundo_nombre_gtemp end) as empleado_responsable,ide_geedp,apellido_paterno_gtemp||' '||primer_nombre_gtemp as elaborado \n" +
+"            from gth_empleado a, gen_empleados_departamento_par b where a.ide_gtemp = b.ide_gtemp \n" +
+"            ) b on a.ide_geedp = b.ide_geedp \n" +
+"            left join pre_poa_tramite c on a.ide_prtra = c.ide_prtra \n" +
+"            left join ( \n" +
+"            select a.ide_pranu,a.ide_prpro,a.ide_geani,nom_geani,valor_reformado_pranu,valor_inicial_pranu,valor_codificado_pranu,valor_devengado_pranu,valor_eje_comprometido_pranu,a.ide_prfuf, \n" +
+"            cod_programa_prpro,detalle_programa,detalle_proyecto,detalle_producto,detalle_actividad,detalle_subactividad,detalle_prfuf,codigo_clasificador_prcla,descripcion_clasificador_prcla \n" +
+"            from pre_anual a, ( \n" +
+"            select a.ide_prpro,cod_programa_prpro,detalle_programa,detalle_proyecto,detalle_producto,detalle_actividad,detalle_subactividad,codigo_clasificador_prcla,descripcion_clasificador_prcla \n" +
+"            from pre_programa a, ( \n" +
+"            select a.ide_prfup,codigo_subactividad,detalle_subactividad,subactividad,detalle_actividad,actividad,detalle_producto,producto,detalle_proyecto, \n" +
+"            			proyecto,detalle_programa ,programa from (select ide_prfup ,pre_ide_prfup,codigo_prfup as codigo_subactividad,detalle_prfup as detalle_subactividad, \n" +
+"            			 detalle_prnfp as subactividad from pre_funcion_programa a, pre_nivel_funcion_programa b where a.ide_prnfp = b.ide_prnfp and a.ide_prnfp =5) a , \n" +
+"            			 (select ide_prfup ,pre_ide_prfup,codigo_prfup as codigo_actividad,detalle_prfup as detalle_actividad,detalle_prnfp as actividad \n" +
+"            			 from pre_funcion_programa a, pre_nivel_funcion_programa b where a.ide_prnfp = b.ide_prnfp and a.ide_prnfp =4) b, \n" +
+"            			 (select ide_prfup ,pre_ide_prfup,codigo_prfup as codigo_producto,detalle_prfup as detalle_producto,detalle_prnfp as producto \n" +
+"            			 from pre_funcion_programa a, pre_nivel_funcion_programa b where a.ide_prnfp = b.ide_prnfp and a.ide_prnfp =3 ) c, \n" +
+"            			 (select ide_prfup ,pre_ide_prfup,codigo_prfup as codigo_proyecto,detalle_prfup as detalle_proyecto,detalle_prnfp as proyecto \n" +
+"            			 from pre_funcion_programa a, pre_nivel_funcion_programa b where a.ide_prnfp = b.ide_prnfp and a.ide_prnfp =2) d, \n" +
+"            			 (select ide_prfup ,pre_ide_prfup,codigo_prfup as codigo_programa,detalle_prfup as detalle_programa,detalle_prnfp as programa \n" +
+"            			 from pre_funcion_programa a, pre_nivel_funcion_programa b where a.ide_prnfp = b.ide_prnfp and a.ide_prnfp =1) e where a.pre_ide_prfup = b.ide_prfup \n" +
+"            			 and b.pre_ide_prfup = c.ide_prfup and c.pre_ide_prfup = d.ide_prfup and d.pre_ide_prfup = e.ide_prfup \n" +
+"            ) b,pre_clasificador c \n" +
+"            where a.ide_prfup = b.ide_prfup \n" +
+"            and a.ide_prcla = c.ide_prcla \n" +
+"            ) b, pre_fuente_financiamiento c,gen_anio d \n" +
+"            where a.ide_prpro = b.ide_prpro \n" +
+"            and a.ide_prfuf = c.ide_prfuf \n" +
+"            and a.ide_geani =d.ide_geani \n" +
+"            order by cod_programa_prpro \n" +
+"            ) d on c.ide_pranu = d.ide_pranu \n" +
+"            left join (select ide_prpot ,sum(valor_adpres) as devengado \n" +
+"            from adq_presupuesto group by ide_adpres ) e on c.ide_prpot = e.ide_prpot\n" +
+"            where not c.ide_prpot is null";
+                return sql;
+    }
+    
     public String getDatosEmpleadoConsulta (String ide_gtemp){
         String sql = "";
         sql= "select ide_gtemp, documento_identidad_gtemp, apellido_paterno_gtemp||' '||(case when apellido_materno_gtemp is null then '' else apellido_materno_gtemp end)||' '||primer_nombre_gtemp ||' '||\n" +
@@ -187,7 +272,58 @@ public class ServiciosAdquisiones {
              "from gth_empleado where ide_gtemp in ("+ide_gtemp+") order by nombres_empleado";
                 return sql;
     }
-
+    public String getDatosProveedorConsulta (String ide_geper){
+        String sql = "";
+        sql= " select ide_geper, identificac_geper, nom_geper from gen_persona  "
+             + "where ide_geper in ("+ide_geper+") order by nom_geper";
+                return sql;
+    }
+    
+    public String getDatosPresupuestoConsulta (String ide_prpot){
+        String sql = "";
+        sql= "select c.ide_prpot,comprometido_prpot,comprometido_prpot  - (case when devengado is null then 0 else devengado end) as saldoxdevengado,fecha_tramite_prtra,cod_programa_prpro,codigo_clasificador_prcla,detalle_actividad,detalle_subactividad,detalle_prfuf,elaborado,observaciones_prtra,a.ide_geedp,empleado_responsable \n" +
+"            from pre_tramite a \n" +
+"            left join \n" +
+"            ( \n" +
+"            select apellido_paterno_gtemp||' '||(case when apellido_materno_gtemp is null then '' else apellido_materno_gtemp end)||' '||primer_nombre_gtemp ||' '|| \n" +
+"            (case when segundo_nombre_gtemp is null then '' else segundo_nombre_gtemp end) as empleado_responsable,ide_geedp,apellido_paterno_gtemp||' '||primer_nombre_gtemp as elaborado \n" +
+"            from gth_empleado a, gen_empleados_departamento_par b where a.ide_gtemp = b.ide_gtemp \n" +
+"            ) b on a.ide_geedp = b.ide_geedp \n" +
+"            left join pre_poa_tramite c on a.ide_prtra = c.ide_prtra \n" +
+"            left join ( \n" +
+"            select a.ide_pranu,a.ide_prpro,a.ide_geani,nom_geani,valor_reformado_pranu,valor_inicial_pranu,valor_codificado_pranu,valor_devengado_pranu,valor_eje_comprometido_pranu,a.ide_prfuf, \n" +
+"            cod_programa_prpro,detalle_programa,detalle_proyecto,detalle_producto,detalle_actividad,detalle_subactividad,detalle_prfuf,codigo_clasificador_prcla,descripcion_clasificador_prcla \n" +
+"            from pre_anual a, ( \n" +
+"            select a.ide_prpro,cod_programa_prpro,detalle_programa,detalle_proyecto,detalle_producto,detalle_actividad,detalle_subactividad,codigo_clasificador_prcla,descripcion_clasificador_prcla \n" +
+"            from pre_programa a, ( \n" +
+"            select a.ide_prfup,codigo_subactividad,detalle_subactividad,subactividad,detalle_actividad,actividad,detalle_producto,producto,detalle_proyecto, \n" +
+"            			proyecto,detalle_programa ,programa from (select ide_prfup ,pre_ide_prfup,codigo_prfup as codigo_subactividad,detalle_prfup as detalle_subactividad, \n" +
+"            			 detalle_prnfp as subactividad from pre_funcion_programa a, pre_nivel_funcion_programa b where a.ide_prnfp = b.ide_prnfp and a.ide_prnfp =5) a , \n" +
+"            			 (select ide_prfup ,pre_ide_prfup,codigo_prfup as codigo_actividad,detalle_prfup as detalle_actividad,detalle_prnfp as actividad \n" +
+"            			 from pre_funcion_programa a, pre_nivel_funcion_programa b where a.ide_prnfp = b.ide_prnfp and a.ide_prnfp =4) b, \n" +
+"            			 (select ide_prfup ,pre_ide_prfup,codigo_prfup as codigo_producto,detalle_prfup as detalle_producto,detalle_prnfp as producto \n" +
+"            			 from pre_funcion_programa a, pre_nivel_funcion_programa b where a.ide_prnfp = b.ide_prnfp and a.ide_prnfp =3 ) c, \n" +
+"            			 (select ide_prfup ,pre_ide_prfup,codigo_prfup as codigo_proyecto,detalle_prfup as detalle_proyecto,detalle_prnfp as proyecto \n" +
+"            			 from pre_funcion_programa a, pre_nivel_funcion_programa b where a.ide_prnfp = b.ide_prnfp and a.ide_prnfp =2) d, \n" +
+"            			 (select ide_prfup ,pre_ide_prfup,codigo_prfup as codigo_programa,detalle_prfup as detalle_programa,detalle_prnfp as programa \n" +
+"            			 from pre_funcion_programa a, pre_nivel_funcion_programa b where a.ide_prnfp = b.ide_prnfp and a.ide_prnfp =1) e where a.pre_ide_prfup = b.ide_prfup \n" +
+"            			 and b.pre_ide_prfup = c.ide_prfup and c.pre_ide_prfup = d.ide_prfup and d.pre_ide_prfup = e.ide_prfup \n" +
+"            ) b,pre_clasificador c \n" +
+"            where a.ide_prfup = b.ide_prfup \n" +
+"            and a.ide_prcla = c.ide_prcla \n" +
+"            ) b, pre_fuente_financiamiento c,gen_anio d \n" +
+"            where a.ide_prpro = b.ide_prpro \n" +
+"            and a.ide_prfuf = c.ide_prfuf \n" +
+"            and a.ide_geani =d.ide_geani \n" +
+"            order by cod_programa_prpro \n" +
+"            ) d on c.ide_pranu = d.ide_pranu \n" +
+"            left join (select ide_prpot ,sum(valor_adpres) as devengado \n" +
+"            from adq_presupuesto group by ide_adpres ) e on c.ide_prpot = e.ide_prpot\n" +
+"            where not c.ide_prpot is null\n" +
+"            and c.ide_prpot in ("+ide_prpot+") ";
+                return sql;
+    }
+    
     public void setUpdateEstadoGastos(int codigo, String dato) {
         String auSql = "UPDATE adq_compra\n"
                 + "set APRUEBA_GASTO_ADCOMP= true,\n"
