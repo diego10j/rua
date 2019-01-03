2958945453ef826b4859db6fcfb22308603016ea
hhslouis <hhsoul_louis@hotmail.com>
2019-01-03 15:26:46 -0500

    modifacnod reportes

---
+++ rua/rua-war/src/java/paq_sistema/pre_reporte_visualizador.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paq_sistema;
+
+import paq_produccion.*;
+import framework.componentes.Division;
+import framework.componentes.PanelTabla;
+import framework.componentes.Tabla;
+import sistema.aplicacion.Pantalla;
+
+/**
+ *
+ * @author Nicolas Cajilema
+ */
+public class pre_reporte_visualizador extends Pantalla {
+     private Tabla tab_color=new Tabla();
+    
+    public  pre_reporte_visualizador (){
+        tab_color.setId("tab_color");
+        tab_color.setTabla("sis_reporte_visual","ide_repv",1);
+        tab_color.dibujar();
+        
+        PanelTabla pat_color = new PanelTabla();
+        pat_color.setId("pat_color");
+        pat_color.setPanelTabla(tab_color);
+        
+        
+        Division div_color = new Division();
+        div_color.setId("div_color");
+        div_color.dividir1(pat_color);
+        
+        agregarComponente(div_color);   
+
+    }
+
+   @Override
+    public void insertar() {
+        tab_color.insertar();
+    }
+
+    @Override
+    public void guardar() {
+        tab_color.guardar();
+        guardarPantalla();
+    }
+
+    @Override
+    public void eliminar() {
+        tab_color.eliminar();
+        
+                
+    }
+
+    public Tabla getTab_color() {
+        return tab_color;
+    }
+
+    public void setTab_color(Tabla tab_color) {
+        this.tab_color = tab_color;
+    }
+    
+    
+}
