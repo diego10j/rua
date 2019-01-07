777fbd345c102e7c0a7605e1f1d47e784f9a747d
Diego Jácome <diego10j.89@gmail.com>
2019-01-07 08:21:39 -0500

    Asiento contable de retención

--- rua/rua-ejb/src/java/servicios/cuentas_x_cobrar/ServicioCuentasCxC.java
+++ rua/rua-ejb/src/java/servicios/cuentas_x_cobrar/ServicioCuentasCxC.java
@@ -1350,4 +1350,15 @@ public class ServicioCuentasCxC extends ServicioBase {
         return ide_ccctr;
     }
 
+    /**
+     * Busca si tiene configuración para hacer guias de remision electronicas
+     *
+     * @return
+     */
+    public boolean isHaceGuiasElectrónicas() {
+        TablaGenerica tag = utilitario.consultar("select ide_ccdaf,es_electronica_ccdaf "
+                + "from cxc_datos_fac where ide_cntdoc = 8 and es_electronica_ccdaf=true and activo_ccdaf=true and ide_sucu=" + utilitario.getVariable("IDE_SUCU"));
+        return !tag.isEmpty();
+    }
+
 }
