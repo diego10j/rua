92dee0fa8932737a5a7297c1c5ceef87dc3128ec
Diego Jácome <diego10j.89@gmail.com>
2019-01-14 14:44:29 -0500

    Asiento contable de retención

--- rua/rua-war/src/java/paq_contabilidad/pre_contabilidad.java
+++ rua/rua-war/src/java/paq_contabilidad/pre_contabilidad.java
@@ -931,7 +931,7 @@ public class pre_contabilidad extends Pantalla {
                     parametro.put("p_patrimonio", utilitario.getVariable("p_con_tipo_cuenta_patrimonio"));
                     TablaGenerica tab_datos = utilitario.consultar("SELECT * FROM sis_empresa e, sis_sucursal s where s.ide_empr=e.ide_empr and s.ide_empr=" + utilitario.getVariable("ide_empr") + " and s.ide_sucu=" + utilitario.getVariable("ide_sucu"));
                     if (tab_datos.getTotalFilas() > 0) {
-                        parametro.put("logo", "/upload/logos/logo_reporte_" + tab_datos.getValor(0, "identificacion_empr") + ".png");
+                        parametro.put("logo", utilitario.getLogoEmpresa());
                         parametro.put("empresa", tab_datos.getValor(0, "nom_empr"));
                         parametro.put("sucursal", tab_datos.getValor(0, "nom_sucu"));
                         parametro.put("direccion", tab_datos.getValor(0, "direccion_sucu"));
@@ -998,7 +998,7 @@ public class pre_contabilidad extends Pantalla {
                     parametro.put("p_patrimonio", utilitario.getVariable("p_con_tipo_cuenta_patrimonio"));
                     TablaGenerica tab_datos = utilitario.consultar("SELECT * FROM sis_empresa e, sis_sucursal s where s.ide_empr=e.ide_empr and s.ide_empr=" + utilitario.getVariable("ide_empr") + " and s.ide_sucu=" + utilitario.getVariable("ide_sucu"));
                     if (tab_datos.getTotalFilas() > 0) {
-                        parametro.put("logo", "/upload/logos/logo_reporte_" + tab_datos.getValor(0, "identificacion_empr") + ".png");
+                        parametro.put("logo", utilitario.getLogoEmpresa());
                         parametro.put("empresa", tab_datos.getValor(0, "nom_empr"));
                         parametro.put("sucursal", tab_datos.getValor(0, "nom_sucu"));
                         parametro.put("direccion", tab_datos.getValor(0, "direccion_sucu"));
@@ -1062,7 +1062,7 @@ public class pre_contabilidad extends Pantalla {
                     parametro.put("p_costos", utilitario.getVariable("p_con_tipo_cuenta_costos"));
                     TablaGenerica tab_datos = utilitario.consultar("SELECT * FROM sis_empresa e, sis_sucursal s where s.ide_empr=e.ide_empr and s.ide_empr=" + utilitario.getVariable("ide_empr") + " and s.ide_sucu=" + utilitario.getVariable("ide_sucu"));
                     if (tab_datos.getTotalFilas() > 0) {
-                        parametro.put("logo", "/upload/logos/logo_reporte_" + tab_datos.getValor(0, "identificacion_empr") + ".png");
+                        parametro.put("logo", utilitario.getLogoEmpresa());
                         parametro.put("empresa", tab_datos.getValor(0, "nom_empr"));
                         parametro.put("sucursal", tab_datos.getValor(0, "nom_sucu"));
                         parametro.put("direccion", tab_datos.getValor(0, "direccion_sucu"));
@@ -1125,7 +1125,7 @@ public class pre_contabilidad extends Pantalla {
                     parametro.put("p_costos", utilitario.getVariable("p_con_tipo_cuenta_costos"));
                     TablaGenerica tab_datos = utilitario.consultar("SELECT * FROM sis_empresa e, sis_sucursal s where s.ide_empr=e.ide_empr and s.ide_empr=" + utilitario.getVariable("ide_empr") + " and s.ide_sucu=" + utilitario.getVariable("ide_sucu"));
                     if (tab_datos.getTotalFilas() > 0) {
-                        parametro.put("logo", "/upload/logos/logo_reporte_" + tab_datos.getValor(0, "identificacion_empr") + ".png");
+                        parametro.put("logo", utilitario.getLogoEmpresa());
                         parametro.put("empresa", tab_datos.getValor(0, "nom_empr"));
                         parametro.put("sucursal", tab_datos.getValor(0, "nom_sucu"));
                         parametro.put("direccion", tab_datos.getValor(0, "direccion_sucu"));
@@ -1222,7 +1222,7 @@ public class pre_contabilidad extends Pantalla {
 
                         TablaGenerica tab_datos = utilitario.consultar("SELECT * FROM sis_empresa e, sis_sucursal s where s.ide_empr=e.ide_empr and s.ide_empr=" + utilitario.getVariable("ide_empr") + " and s.ide_sucu=" + utilitario.getVariable("ide_sucu"));
                         if (tab_datos.getTotalFilas() > 0) {
-                            parametro.put("logo", "/upload/logos/logo_reporte_" + tab_datos.getValor(0, "identificacion_empr") + ".png");
+                            parametro.put("logo", utilitario.getLogoEmpresa());
                             parametro.put("empresa", tab_datos.getValor(0, "nom_empr"));
                             parametro.put("sucursal", tab_datos.getValor(0, "nom_sucu"));
                             parametro.put("direccion", tab_datos.getValor(0, "direccion_sucu"));
