31029d60884f44c0381b962ffb70284e89ca9451
hhslouis <hhsoul_louis@hotmail.com>
2019-05-20 22:59:21 -0500

    titulacion

--- rua/rua-war/src/java/paq_gestion/pre_empleado_rubro.java
+++ rua/rua-war/src/java/paq_gestion/pre_empleado_rubro.java
@@ -24,9 +24,24 @@ public class pre_empleado_rubro extends Pantalla {
 
     public pre_empleado_rubro() {
 
+        Boton bot_limpiar = new Boton();
+        bot_limpiar.setIcon("ui-icon-cancel");
+        bot_limpiar.setMetodo("limpiar");
+        aut_empleado.setId("aut_empleado");
+        aut_empleado.setAutoCompletar("select IDE_GTEMP,DOCUMENTO_IDENTIDAD_GTEMP, APELLIDO_PATERNO_GTEMP, APELLIDO_MATERNO_GTEMP,PRIMER_NOMBRE_GTEMP,SEGUNDO_NOMBRE_GTEMP  from GTH_EMPLEADO");
+        aut_empleado.setMetodoChange("filtrarEmpleado");
+
+        bar_botones.agregarComponente(aut_empleado);
+        bar_botones.agregarBoton(bot_limpiar);
+        
+        
         tab_tabla.setId("tab_tabla");
         tab_tabla.setTabla("gth_empleado_rubro", "ide_gtemr", 0);
-
+        tab_tabla.setCondicion("ide_gtemp=-1");
+        tab_tabla.getColumna("ide_gtemp").setVisible(false);
+        tab_tabla.getColumna("IDE_NRRUB").setCombo("select RUB.IDE_NRRUB,RUB.DETALLE_NRRUB,FCA.DETALLE_NRFOC,TRU.DETALLE_NRTIR from NRH_RUBRO RUB  " +
+				"LEFT JOIN NRH_FORMA_CALCULO FCA ON FCA.IDE_NRFOC=RUB.IDE_NRFOC " +
+				"LEFT JOIN NRH_TIPO_RUBRO TRU ON TRU.IDE_NRTIR=RUB.IDE_NRTIR") ;
         tab_tabla.dibujar();
 
         PanelTabla pat_panel = new PanelTabla();
@@ -38,21 +53,22 @@ public class pre_empleado_rubro extends Pantalla {
 
         agregarComponente(div_division);
 
-        Boton bot_limpiar = new Boton();
-        bot_limpiar.setIcon("ui-icon-cancel");
-        bot_limpiar.setMetodo("limpiar");
-        aut_empleado.setId("aut_empleado");
-        aut_empleado.setAutoCompletar("select IDE_GTEMP,DOCUMENTO_IDENTIDAD_GTEMP, APELLIDO_PATERNO_GTEMP, APELLIDO_MATERNO_GTEMP,PRIMER_NOMBRE_GTEMP,SEGUNDO_NOMBRE_GTEMP  from GTH_EMPLEADO");
-        aut_empleado.setMetodoChange("filtrarEmpleado");
-
-        bar_botones.agregarComponente(aut_empleado);
-        bar_botones.agregarBoton(bot_limpiar);
     }
-
+    public void filtrarEmpleado(){
+        String empleado=aut_empleado.getValor();
+        tab_tabla.setCondicion("ide_gtemp="+empleado);
+        tab_tabla.ejecutarSql();
+        
+    }
+    public void limpiar(){
+        aut_empleado.limpiar();
+        tab_tabla.limpiar();
+    }
     @Override
     public void insertar() {
 
         tab_tabla.insertar();
+        tab_tabla.setValor("ide_gtemp", aut_empleado.getValor());
 
     }
 
