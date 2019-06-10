36f2a6697d7bbca76e76a20f023dbac25cb1368b
hhslouis <hhsoul_louis@hotmail.com>
2019-06-10 09:20:36 -0500

    parametros

--- rua/rua-ejb/src/java/sistema/parametros/Parametros.java
+++ rua/rua-ejb/src/java/sistema/parametros/Parametros.java
@@ -123,6 +123,8 @@ public class Parametros {
         lis_parametros.add(new Parametro("5", "p_gen_tipo_identificacion_ruc", "Indica el tipo de identificacion que sea RUC ", "1", "gen_tipo_identifi", "ide_getid", "nombre_getid"));
         lis_parametros.add(new Parametro("5", "p_gen_tipo_identificacion_cedula", "Indica el tipo de identificacion que sea CEDULA ", "0", "gen_tipo_identifi", "ide_getid", "nombre_getid"));
         lis_parametros.add(new Parametro("5", "p_com_estado_proveedor", "Indica el estado del Proveedor", "0", "com_estado_provee", "ide_coepr", "nombre_coepr"));
+        lis_parametros.add(new Parametro("5", "p_com_sisfinanciero_forma_pago", "Indica qel tipo de compra y pago con sistema financiero", "2", "con_deta_forma_pago", "ide_cndfp", "nombre_cndfp"));
+        lis_parametros.add(new Parametro("5", "p_com_sustento_bodega", "Indica el sustento tributario por defecto regsitran desde bodegas ", "1", "sri_tipo_sustento_tributario", "ide_srtst", "nombre_srtst"));
 
         /*
          * NOMINA =6
