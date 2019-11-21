dbb29ab20e599d8d807689030263c59577908218
hhslouis <hhsoul_louis@hotmail.com>
2019-11-20 20:03:04 -0500

    perisisten luis toapanta

--- rua/rua-ejb/src/java/paq_gerencial/ejb/ServicioGerencial.java
+++ rua/rua-ejb/src/java/paq_gerencial/ejb/ServicioGerencial.java
@@ -6,6 +6,7 @@
 package paq_gerencial.ejb;
 
 import javax.ejb.Stateless;
+
 /**
  *
  * @author Lusi Toapanta
@@ -14,18 +15,42 @@ import javax.ejb.Stateless;
 
 public class ServicioGerencial {
 
- 
     /**
-     * Retorna sentencia SQL para obtener los nombres de las casas,
-     * y obras salesianas para proceder a autorizar la conexión.
+     * Retorna sentencia SQL para obtener los nombres de las casas, y obras
+     * salesianas para proceder a autorizar la conexión.
      *
      * @return sql
      */
     public String getSqlGerPermiso() {
-        String sql="select ide_geperm,nom_casa_geperm,nom_persistencia_geperm from ger_permiso "
+        String sql = "select ide_geperm,nom_casa_geperm,nom_persistencia_geperm from ger_permiso "
                 + "order by nom_casa_geperm";
         return sql;
     }
 
-    
-}
+    /**
+     * Retorna sentencia SQL para obtener los estados en el que se encuentra, en
+     * el balance general.
+     *
+     * @return sql
+     */
+    public String getEstado() {
+        String sql = "select ide_gerest, detalle_gerest from ger_estado  ";
+        return sql;
+    }
+
+    /**
+     * Retorna sentencia SQL para obtener el nombre y la obra que encuentra, en
+     * desarrollo.
+     *
+     * @return sql
+     */
+    public String getObra() {
+        String sql = "select ide_gerobr, nombre_gerobr, codigo_gerobr, abreviatura_gerobr from ger_obra  ";
+        return sql;
+    }
+
+    public String getTipoBalance() {
+        String sql = "select ide_getiba, detalle_getiba from ger_tipo_balance ";
+        return sql;
+    }
+}   
