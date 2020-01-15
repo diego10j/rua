5245dd9616601fae8b079734799ef23f4d340303
hhslouis <hhsoul_louis@hotmail.com>
2020-01-14 20:13:33 -0500

    servicio inversiones

--- rua/rua-ejb/src/java/paq_gerencial/ejb/ServicioGerencial.java
+++ rua/rua-ejb/src/java/paq_gerencial/ejb/ServicioGerencial.java
@@ -52,9 +52,30 @@ public class ServicioGerencial {
      * @return sql
      */
     public String getAnio(String estado) {
-        String sql = "select ide_geani,nom_geani,activo_geani from gen_anio where activo_geani in ("+estado+") order by nom_geani";
+        String sql = "select ide_geani,nom_geani,(case when activo_geani=true then 'ACTIVO' else 'INACTIVO' end) as activo_geani from gen_anio where activo_geani in ("+estado+") order by nom_geani";
         return sql;
-    }    
+    }
+    /**
+     * Retorna sentencia SQL para obtener el Año
+     * 
+     * @param estado recibe el estado apertura cierre
+     * @param anio recibe el anio fiscal
+     * @param tipo recibe el parametro del tipo 1 si craga toda la lista del sql
+     * @param codigo los ides de las casas y obras por año
+     * @return sql
+     */
+    public String getCasaObraPeriodoFiscal(String estado,String anio,String tipo,String codigo) {
+        String sql = "select ide_gecobc,nombre_gercas,nombre_gerobr from ger_cont_balance_cabecera a,(select a.ide_gercas,nombre_gercas,ide_gerobr,nombre_gerobr " +
+                " from ger_casa a, ger_obra b where a.ide_gercas=b.ide_gercas ) b where a.ide_gerobr=b.ide_gerobr ";
+                if(tipo.equals("1")){
+                 sql+=" and a.ide_geani="+anio+" and a.ide_gerest="+estado;
+                }
+                if(tipo.equals("2")){
+                  sql+=" and ide_gecobc in ("+codigo+")";  
+                }
+                sql +=" order by nombre_gercas,nombre_gerobr";
+        return sql;
+    }      
     /**
      * Retorna sentencia SQL para obtener el nombre de Casa y la Obra
      * 
