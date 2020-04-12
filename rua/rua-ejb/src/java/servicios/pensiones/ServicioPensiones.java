806e7e7729d9ecad10d4dd510ec6df39712b569d
hhslouis <hhsoul_louis@hotmail.com>
2020-04-12 16:51:41 -0500

    sistema gerencial

--- rua/rua-ejb/src/java/servicios/pensiones/ServicioPensiones.java
+++ rua/rua-ejb/src/java/servicios/pensiones/ServicioPensiones.java
@@ -135,10 +135,13 @@ public class ServicioPensiones extends ServicioBase {
             tab_info_adicional.setValor("nombre_srina", "Período Lectivo");
             tab_info_adicional.setValor("valor_srina", tag.getValor("periodo_lectivo_petlf"));
             tab_info_adicional.setValor("ide_cccfa", String.valueOf(ide_cccfa));
-            //tab_info_adicional.insertar();
-            //tab_info_adicional.setValor("nombre_srina", "Recibo");
-           // tab_info_adicional.setValor("valor_srina", tag.getValor("cod_factura_petlf"));
-            //tab_info_adicional.setValor("ide_cccfa", String.valueOf(ide_cccfa));
+            if(utilitario.getVariable("p_pen_dat_adicional").equals("true")){
+            tab_info_adicional.insertar();// para el porcentaje de descuento
+            tab_info_adicional.setValor("nombre_srina", utilitario.getVariable("p_pen_nom_adicional"));
+            tab_info_adicional.setValor("valor_srina", utilitario.getVariable("p_pen_val_adicional"));
+            tab_info_adicional.setValor("ide_cccfa", String.valueOf(ide_cccfa));
+            }
+            
             tab_info_adicional.insertar();
             tab_info_adicional.setValor("nombre_srina", "Usuario");
             tab_info_adicional.setValor("valor_srina", utilitario.getVariable("NICK"));
