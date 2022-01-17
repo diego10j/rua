92da49e01da65ca898c07d3bbcf2af80c5d160ab
hhslouis <hhsoul_louis@hotmail.com>
2022-01-17 18:38:49 -0500

    Actualizando servicio parametros estado canvcelado comprobantes inventario

--- rua/rua-ejb/src/java/sistema/parametros/Parametros.java
+++ rua/rua-ejb/src/java/sistema/parametros/Parametros.java
@@ -64,6 +64,7 @@ public class Parametros {
         lis_parametros.add(new Parametro("1", "p_inv_valor_base", "Permite conocer el valor base del articulo", "1"));
         lis_parametros.add(new Parametro("1", "p_inv_porcenatje_utilidad", "Permite conocer el porcentaje de utilidad del articulo", "2"));
         lis_parametros.add(new Parametro("1", "p_inv_estado_aprobado", "Indica el estado aprobado ", "2", "inv_est_prev_inve", "ide_inepi", "nombre_inepi"));
+        lis_parametros.add(new Parametro("1", "p_inv_estado_anulado", "Indica el estado anulado ", "2", "inv_est_prev_inve", "ide_inepi", "nombre_inepi"));
         lis_parametros.add(new Parametro("1", "p_inv_ingreso_compra", "Indica las compras ingresadas por facturas compras ", "0", "inv_tip_tran_inve", "ide_intti", "nombre_intti"));
         lis_parametros.add(new Parametro("1", "p_inv_grupo_entrega_productos", "Indica los articulos solo de los grupos seleccionados", "1", "inv_articulo", "ide_inarti", "nombre_inarti"));
        lis_parametros.add(new Parametro("1", "p_inv_tipo_comprobante", "Indica el tipo de comprobante de inventario ", "0", "con_tipo_comproba", "ide_cntcm", "nombre_cntcm"));
